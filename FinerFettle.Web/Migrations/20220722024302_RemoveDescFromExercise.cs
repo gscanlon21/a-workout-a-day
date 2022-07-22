45f780f4ee68485e4c88ef3ae092bf258427bfe6
Graham Scanlon <gscanlon21@gmail.com>
2022-07-21 23:05:33 -0500

    improvement(exercise): remove desc
    Former-commit-id: 15581652d76e7d0496029b5e5d756ccc7921b34e

---
+++ FinerFettle.Web/Migrations/20220722024302_RemoveDescFromExercise.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinerFettle.Web.Migrations
+{
+    public partial class RemoveDescFromExercise : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Exercise");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Exercise",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
