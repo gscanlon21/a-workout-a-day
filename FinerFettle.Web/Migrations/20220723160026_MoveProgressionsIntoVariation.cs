08cb984a9d7d2600216ce8f79b269d49b1c499f7
Graham Scanlon <gscanlon21@gmail.com>
2022-07-23 11:50:41 -0500

    fix: workout intensity

---
+++ FinerFettle.Web/Migrations/20220723160026_MoveProgressionsIntoVariation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinerFettle.Web.Migrations
+{
+    public partial class MoveProgressionsIntoVariation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Progression",
+                table: "Intensity");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Progression",
+                table: "Variation",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Progression",
+                table: "Variation");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Progression",
+                table: "Intensity",
+                type: "integer",
+                nullable: true);
+        }
+    }
+}
--- FinerFettle.Web/Models/Exercise/Intensity.cs
+++ FinerFettle.Web/Models/Exercise/Intensity.cs
@@ -4,17 +4,47 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FinerFettle.Web.Models.Exercise
 {
-    [Comment("Intensity level of an exercise variation"), Table(nameof(Intensity))]
+    [Table(nameof(Intensity)), Comment("Intensity level of an exercise variation")]
     public class Intensity
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
+        public Proficiency Proficiency { get; set; } = null!;
+
+        [Required]
         public IntensityLevel IntensityLevel { get; set; }
 
         [Required]
-        public Proficiency Proficiency { get; set; } = null!;
+        public MuscleContractions MuscleContractions { get; set; }
+
+        public string GetHarderVariation()
+        {
+            if (!MuscleContractions.HasFlag(MuscleContractions.Isometric) && Proficiency.Secs != null)
+            {
+                return $"For a harder workout, add a {Proficiency.Secs} second hold at the peak of each repetition";
+            }
+
+            return string.Empty;
+        }
+
+        public string GetProficiency()
+        {
+            if (Proficiency.Sets.HasValue && Proficiency.Reps.HasValue && Proficiency.Secs.HasValue && MuscleContractions.HasFlag(MuscleContractions.Isometric))
+            {
+                return $"{Proficiency.Sets} set(s) of {Proficiency.Reps} reps with a {Proficiency.Secs} second hold";
+            }
+            else if (Proficiency.Sets.HasValue && Proficiency.Reps.HasValue)
+            {
+                return $"{Proficiency.Sets} set(s) of {Proficiency.Reps} reps";
+            }
+            else if (Proficiency.Sets.HasValue && Proficiency.Secs.HasValue)
+            {
+                return $"{Proficiency.Sets} set(s) of a {Proficiency.Secs} second hold";
+            }
+            return String.Empty;
+        }
     }
 
     public enum IntensityLevel
@@ -24,23 +54,5 @@ namespace FinerFettle.Web.Models.Exercise
     }
 
     [Owned]
-    public record Proficiency(int? Sets, int? Reps, int? Secs)
-    {
-        public override string ToString()
-        {
-            if (Sets.HasValue && Reps.HasValue && Secs.HasValue)
-            {
-                return $"{Sets} set(s) of {Reps} reps with a {Secs} sec hold";
-            }
-            else if (Sets.HasValue && Reps.HasValue)
-            {
-                return $"{Sets} set(s) of {Reps} reps";
-            }
-            else if (Sets.HasValue && Secs.HasValue)
-            {
-                return $"{Sets} set(s) of a {Secs} sec hold";
-            }
-            return String.Empty;
-        }
-    }
+    public record Proficiency(int? Sets, int? Reps, int? Secs);
 }
