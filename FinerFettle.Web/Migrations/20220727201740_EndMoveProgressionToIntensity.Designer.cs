1aa3ac2b519c6f95c8d7eab9755136da43d52dc0
Graham Scanlon <gscanlon21@gmail.com>
2022-07-27 18:05:53 -0500

    refactor: exercise progressions & muscle contractions

--- FinerFettle.Web/Migrations/20220727201740_EndMoveProgressionToIntensity.Designer.cs
+++ FinerFettle.Web/Migrations/20220727201740_EndMoveProgressionToIntensity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FinerFettle.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FinerFettle.Web.Migrations
 {
     [DbContext(typeof(CoreContext))]
-    partial class WorkoutContextModelSnapshot : ModelSnapshot
+    [Migration("20220727201740_EndMoveProgressionToIntensity")]
+    partial class EndMoveProgressionToIntensity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -159,9 +161,6 @@ namespace FinerFettle.Web.Migrations
                     b.Property<string>("Code")
                         .IsRequired()
                         .HasColumnType("text");
-
-                    b.Property<bool>("Enabled")
-                        .HasColumnType("boolean");
 
                     b.Property<int?>("ExerciseId")
                         .HasColumnType("integer");
