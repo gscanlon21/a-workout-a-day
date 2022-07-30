8c3ff4c479d1256c8485023aa960b6e9fe9d4c48
Graham Scanlon <gscanlon21@gmail.com>
2022-07-29 20:52:02 -0500

    feat: add view all exercises

--- FinerFettle.Web/Controllers/ExerciseController.cs
+++ FinerFettle.Web/Controllers/ExerciseController.cs
@@ -27,52 +27,43 @@ namespace FinerFettle.Web.Controllers
             // TODO: Refactor
 
             // Flatten all exercise variations and intensities into one big list
-            var query = _context.Exercises
-                .SelectMany(e => e.Variations
-                .Where(v => v.Enabled).Select(v =>
-                new {
-                    Variation = v,
-                    e.Muscles,
-                    e.ExerciseType,
-                    Intensities = v.Intensities.Select(i => new { i, i.Proficiency }),
-                    EquipmentGroups = v.EquipmentGroups.Select(e => new { e, e.Equipment })
-                }));
-                 // Select a random subset of exercises
-
-            
-            
-            var temp = (await query
-                .ToListAsync());
-
-            var allExercises = temp
-                    // Make sure the user owns all the equipment necessary for the exercise
-                    .SelectMany(v => v.Intensities
-                        .Select(i => new {
-                            Variation = v.Variation,
-                            Intensity = i.i, // Need to select into an anonymous object so Proficiency is included...
-                            Muscles = v.Muscles,
-                            ExerciseType = v.ExerciseType
-                        }))
-                .Select(a => new ExerciseViewModel(a.Variation, a.Intensity, a.Muscles, a.ExerciseType));
+            var allExercises = (await _context.Variations
+                .Include(v => v.Exercise)
+                .Include(v => v.EquipmentGroups)
+                .ThenInclude(e => e.Equipment)
+                .Where(v => v.Enabled)
+                .SelectMany(v => v.Intensities
+                    .Select(i => new {
+                        Variation = v,
+                        Intensity = i, // Need to select into an anonymous object so Proficiency is included...
+                        Muscles = v.Exercise.Muscles,
+                        ExerciseType = v.Exercise.ExerciseType
+                    }))
+                .Select(a => new ExerciseViewModel(a.Variation, a.Intensity, a.Muscles, a.ExerciseType))
+                .ToListAsync())
+                .OrderBy(e => e.Exercise.Exercise.Code)
+                .ThenBy(e => e.Intensity.MinProgression)
+                .ThenBy(e => e.Intensity.MaxProgression);
 
             var exercises = allExercises
                 // Make sure the exercise is the correct type and not a warmup exercise
                 .Where(e => e.ActivityLevel == ExerciseActivityLevel.Main)
                 .ToList();
 
-            var viewModel = new NewsletterViewModel(exercises);
+            var viewModel = new NewsletterViewModel(exercises)
+            {
+                WarmupExercises = allExercises
+                    // Choose dynamic stretches for warmup
+                    .Where(e => e.ActivityLevel == ExerciseActivityLevel.Warmup)
+                    .ToList(),
 
-            viewModel.WarmupExercises = allExercises
-                // Choose dynamic stretches for warmup
-                .Where(e => e.ActivityLevel == ExerciseActivityLevel.Warmup)
-                .ToList();
+                CooldownExercises = allExercises
+                    // Choose static stretches for cooldown
+                    .Where(e => e.ActivityLevel == ExerciseActivityLevel.Cooldown)
+                    .ToList()
+            };
 
-            viewModel.CooldownExercises = allExercises
-                // Choose static stretches for cooldown
-                .Where(e => e.ActivityLevel == ExerciseActivityLevel.Cooldown)
-                .ToList();
-
-            return View("All", viewModel);
+            return View(viewModel);
         }
     }
 }
