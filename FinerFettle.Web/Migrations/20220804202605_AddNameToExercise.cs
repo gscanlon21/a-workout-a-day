784cb2d9037ca9465936bc5f6779c4b63875627e
Graham Scanlon <gscanlon21@gmail.com>
2022-08-04 21:52:28 -0500

    feat: add strengthening user preference and minor email validation

---
+++ FinerFettle.Web/Migrations/20220804202605_AddNameToExercise.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinerFettle.Web.Migrations
+{
+    public partial class AddNameToExercise : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Exercise",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Exercise");
+        }
+    }
+}
