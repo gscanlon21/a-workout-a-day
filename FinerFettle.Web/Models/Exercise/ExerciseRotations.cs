eb2267ddd66115e3122a31cd581e465cb95e3063
Graham Scanlon <gscanlon21@gmail.com>
2022-08-04 21:51:41 -0500

    fix: exercise rotation equals identification
    Former-commit-id: 14fea08a1fcbcb2752e4e9e6aaa86f97598ac8f9

--- FinerFettle.Web/Models/Exercise/ExerciseRotations.cs
+++ FinerFettle.Web/Models/Exercise/ExerciseRotations.cs
@@ -5,5 +5,5 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace FinerFettle.Web.Models.Exercise
 {
     [Owned]
-    public record ExerciseRotaion(ExerciseType ExerciseType, MuscleGroups MuscleGroups);
+    public record ExerciseRotaion(int id, ExerciseType ExerciseType, MuscleGroups MuscleGroups);
 }
