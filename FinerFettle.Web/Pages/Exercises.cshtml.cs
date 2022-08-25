1b51ae9006dcb3dc90b4b7318c6327f9d5f6bb7e
Graham Scanlon <gscanlon21@gmail.com>
2022-08-25 15:52:01 -0500

    feat: add demo content to landing page
    Former-commit-id: 14b72406024207fd8fcc4784315a2f43fd04afe3

---
+++ FinerFettle.Web/Pages/Exercises.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinerFettle.Web.Pages
+{
+    public class ExercisesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
