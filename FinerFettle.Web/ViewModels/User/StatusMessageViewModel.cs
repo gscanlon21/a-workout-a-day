71fe7053abe2c81a715624db51c79dda89e18213
Graham Scanlon <gscanlon21@gmail.com>
2022-08-29 18:13:36 -0500

    refactor: move status message auto-close duration into model

---
+++ FinerFettle.Web/ViewModels/User/StatusMessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FinerFettle.Web.ViewModels.User
+{
+    public class StatusMessageViewModel
+    {
+        public StatusMessageViewModel(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; init; }
+        public int? AutoCloseInXSeconds { get; init; } = 9;
+    }
+}
