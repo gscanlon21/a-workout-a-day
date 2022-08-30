35a51f808759a2c7ed2c4cdf79e162f5216c3c25
Graham Scanlon <gscanlon21@gmail.com>
2022-08-30 18:17:37 -0500

    improvement: remove unnecessary log

--- FinerFettle.Web/wwwroot/js/validation/mustbetrue.js
+++ FinerFettle.Web/wwwroot/js/validation/mustbetrue.js
@@ -1,5 +1,4 @@
-﻿console.log('Addiong Validation');
-$.validator.addMethod("mustbetrue", function (value, element, param) {
+﻿$.validator.addMethod("mustbetrue", function (value, element, param) {
     return element.checked;
 });
 $.validator.unobtrusive.adapters.addBool("mustbetrue");
