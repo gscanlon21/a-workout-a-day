fff67dfbcaf9fc2775ba4b3a32ae3b8f74f5d283
Graham Scanlon <gscanlon21@gmail.com>
2022-08-30 12:40:19 -0500

    improvement: hide equipment that the user doesn't own
    Former-commit-id: 9db6dac5d00956a1270862a14ab4ffe0831f24a2

---
+++ FinerFettle.Web/ViewModels/Newsletter/EquipmentGroupViewModel.cs
@@ -0,0 +1,16 @@
+﻿using FinerFettle.Web.Models.Exercise;
+
+namespace FinerFettle.Web.ViewModels.Newsletter
+{
+    public class EquipmentGroupViewModel
+    {
+        public EquipmentGroupViewModel(EquipmentGroup equipmentGroup, Models.User.User? user)
+        {
+            EquipmentGroup = equipmentGroup;
+            User = user;
+        }
+
+        public EquipmentGroup EquipmentGroup { get; init; }
+        public Models.User.User? User { get; init; }
+    }
+}
