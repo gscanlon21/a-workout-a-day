d220363e63f0f727931a6623cc0cdfbe02b27992
Graham Scanlon <gscanlon21@gmail.com>
2022-08-31 18:22:32 -0500

    improvement: order of shuffled exercises
    Former-commit-id: b3a37517d95c96c363ce49b8e0f6329eb248e643

---
+++ FinerFettle.Web/Extensions/ThreadSafeRandom.cs
@@ -0,0 +1,13 @@
+﻿namespace FinerFettle.Web.Extensions
+{
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic] 
+        private static Random? Local;
+
+        public static Random ThisThreadsRandom
+        {
+            get => Local ??= new Random(unchecked(Environment.TickCount * 31 + Environment.CurrentManagedThreadId));
+        }
+    }
+}
