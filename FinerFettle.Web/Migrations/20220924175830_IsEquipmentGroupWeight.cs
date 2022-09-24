d189f08084ec1ea005a7e0d91c859f4ac13075aa
Graham Scanlon <gscanlon21@gmail.com>
2022-09-24 13:02:18 -0500

    fix: sets/res calculation

---
+++ FinerFettle.Web/Migrations/20220924175830_IsEquipmentGroupWeight.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinerFettle.Web.Migrations
+{
+    public partial class IsEquipmentGroupWeight : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsWeight",
+                table: "Equipment");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWeight",
+                table: "EquipmentGroup",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsWeight",
+                table: "EquipmentGroup");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWeight",
+                table: "Equipment",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
