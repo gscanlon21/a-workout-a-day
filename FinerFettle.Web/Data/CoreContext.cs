32561ab40bb2b788ce5fb84b54f54be439134c25
Graham Scanlon <gscanlon21@gmail.com>
2022-10-04 00:07:20 -0500

    refactor: make disabled filters apply globally
    Former-commit-id: 4f17e36ec56a6609e46f15a3423153f59b9db2e6

--- FinerFettle.Web/Data/CoreContext.cs
+++ FinerFettle.Web/Data/CoreContext.cs
@@ -3,6 +3,7 @@ using FinerFettle.Web.Models.User;
 using FinerFettle.Web.Models.Exercise;
 using Microsoft.EntityFrameworkCore;
 using FinerFettle.Web.Models.Footnotes;
+using Microsoft.Extensions.Hosting;
 
 namespace FinerFettle.Web.Data
 {
@@ -11,7 +12,10 @@ namespace FinerFettle.Web.Data
         public DbSet<User> Users { get; set; } = null!;
         public DbSet<Equipment> Equipment { get; set; } = null!;
         public DbSet<ExerciseUserProgression> UserProgressions { get; set; } = null!;
+        public DbSet<UserVariation> UserVariations { get; set; } = null!;
+        public DbSet<UserIntensity> UserIntensities { get; set; } = null!;
         public DbSet<Intensity> Intensities { get; set; } = null!;
+        public DbSet<Exercise> Exercises { get; set; } = null!;
         public DbSet<Newsletter> Newsletters { get; set; } = null!;
         public DbSet<Footnote> Footnotes { get; set; } = null!;
 
@@ -21,8 +25,19 @@ namespace FinerFettle.Web.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<EquipmentUser>().HasKey(sc => new { sc.EquipmentId, sc.UserId });
-            modelBuilder.Entity<ExerciseUserProgression>().HasKey(sc => new { sc.ExerciseId, sc.UserId });
+            modelBuilder.Entity<EquipmentUser>().HasKey(sc => new { sc.UserId, sc.EquipmentId });
+            modelBuilder.Entity<ExerciseUserProgression>().HasKey(sc => new { sc.UserId, sc.ExerciseId });
+            modelBuilder.Entity<UserVariation>().HasKey(sc => new { sc.UserId, sc.VariationId });
+            modelBuilder.Entity<UserIntensity>().HasKey(sc => new { sc.UserId, sc.IntensityId });
+
+            modelBuilder.Entity<Intensity>().HasQueryFilter(p => p.DisabledReason == null);
+            modelBuilder.Entity<Variation>().HasQueryFilter(p => p.DisabledReason == null);
+            modelBuilder.Entity<Exercise>().HasQueryFilter(p => p.DisabledReason == null);
+            modelBuilder.Entity<UserVariation>().HasQueryFilter(p => p.Variation.DisabledReason == null);
+            modelBuilder.Entity<UserIntensity>().HasQueryFilter(p => p.Intensity.DisabledReason == null);
+            modelBuilder.Entity<ExerciseUserProgression>().HasQueryFilter(p => p.Exercise.DisabledReason == null);
+            modelBuilder.Entity<IntensityPreference>().HasQueryFilter(p => p.Intensity.DisabledReason == null);
+            modelBuilder.Entity<EquipmentGroup>().HasQueryFilter(p => p.Intensity.DisabledReason == null);
         }
     }
 }
