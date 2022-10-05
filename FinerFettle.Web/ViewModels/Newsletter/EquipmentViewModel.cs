96e978fb421db6951cd198c97de6ee45b0da0a94
Graham Scanlon <gscanlon21@gmail.com>
2022-10-05 18:01:39 -0500

    improvement(newsletter): show user's owned and un-owned equipment

---
+++ FinerFettle.Web/ViewModels/Newsletter/EquipmentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using FinerFettle.Web.Models.Exercise;
+
+namespace FinerFettle.Web.ViewModels.Newsletter
+{
+    public class EquipmentViewModel
+    {
+        public EquipmentViewModel(IEnumerable<Equipment> allEquipment, IEnumerable<Equipment> userEquipment)
+        {
+            AllEquipment = allEquipment;
+            UserEquipment = userEquipment;
+        }
+
+        public IEnumerable<Equipment> AllEquipment { get; init; }
+        public IEnumerable<Equipment> UserEquipment { get; init; }
+    }
+}
