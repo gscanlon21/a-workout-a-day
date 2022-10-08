837ff88ae6b58680921513f41f83a01abcdb4ae3
Graham Scanlon <gscanlon21@gmail.com>
2022-10-07 20:14:10 -0500

    feat: allow setting pre-reqs for an exercise - before they are shown to the user, the user must hit the target proficiency level for a different exercise

---
+++ FinerFettle.Web/Migrations/20221007145515_ExerciseRequisite.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinerFettle.Web.Migrations
+{
+    public partial class ExerciseRequisite : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "exercise_requisite",
+                columns: table => new
+                {
+                    ExerciseId = table.Column<int>(type: "integer", nullable: false),
+                    RequiresProficiencyOfId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_exercise_requisite", x => new { x.ExerciseId, x.RequiresProficiencyOfId });
+                    table.ForeignKey(
+                        name: "FK_exercise_requisite_Exercise_ExerciseId",
+                        column: x => x.ExerciseId,
+                        principalTable: "Exercise",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_exercise_requisite_Exercise_RequiresProficiencyOfId",
+                        column: x => x.RequiresProficiencyOfId,
+                        principalTable: "Exercise",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                },
+                comment: "Pre-requisite exercises for other exercises");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_exercise_requisite_RequiresProficiencyOfId",
+                table: "exercise_requisite",
+                column: "RequiresProficiencyOfId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "exercise_requisite");
+        }
+    }
+}
