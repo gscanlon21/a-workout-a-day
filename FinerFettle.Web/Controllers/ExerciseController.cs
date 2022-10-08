66dea4f28d60a32cb5d786ab38a1e5b77b594e6a
Graham Scanlon <gscanlon21@gmail.com>
2022-10-07 20:07:58 -0500

    feat: exercise data check debug page
    Former-commit-id: 8db7a548bf067fc08eb2cb87dbceb18c91cb7813

--- FinerFettle.Web/Controllers/ExerciseController.cs
+++ FinerFettle.Web/Controllers/ExerciseController.cs
@@ -1,4 +1,5 @@
 ﻿using FinerFettle.Web.Data;
+using FinerFettle.Web.Models.Newsletter;
 using FinerFettle.Web.ViewModels.Newsletter;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -27,21 +28,55 @@ namespace FinerFettle.Web.Controllers
             var allExercises = (await _context.Intensities
                 .Include(i => i.Variation)
                     .ThenInclude(v => v.Exercise)
+                        .ThenInclude(e => e.Prerequisites)
+                            .ThenInclude(e => e.PrerequisiteExercise)
                 .Include(i => i.EquipmentGroups)
                     .ThenInclude(eg => eg.Equipment)
                 .Include(i => i.IntensityPreferences)
                 .Select(i => new ExerciseViewModel(null, i.Variation.Exercise, i.Variation, i)
                 {
-                    Verbosity = Models.Newsletter.Verbosity.Diagnostic
+                    Verbosity = Verbosity.Diagnostic
                 })
                 .ToListAsync())
-                .OrderBy(vm => vm.Exercise.Id) // OrderBy must come after query or you get duplicates
+                .OrderBy(vm => vm.Exercise.Name) // OrderBy must come after query or you get duplicates
                 .ThenBy(vm => vm.Intensity.Progression.Min)
                 .ThenBy(vm => vm.Intensity.Progression.Max == null)
                 .ThenBy(vm => vm.Intensity.Progression.Max);
 
             var exercises = allExercises.Where(vm => vm.ActivityLevel == ExerciseActivityLevel.Main).ToList();
-            var viewModel = new NewsletterViewModel(exercises, Models.Newsletter.Verbosity.Diagnostic)
+            var viewModel = new NewsletterViewModel(exercises, Verbosity.Diagnostic)
+            {
+                WarmupExercises = allExercises.Where(vm => vm.ActivityLevel == ExerciseActivityLevel.Warmup).ToList(),
+                CooldownExercises = allExercises.Where(vm => vm.ActivityLevel == ExerciseActivityLevel.Cooldown).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("check")]
+        public async Task<IActionResult> Check()
+        {
+            // Flatten all exercise variations and intensities into one big list
+            var allExercises = (await _context.Intensities
+                .Include(i => i.Variation)
+                    .ThenInclude(v => v.Exercise)
+                        .ThenInclude(e => e.Prerequisites)
+                            .ThenInclude(e => e.PrerequisiteExercise)
+                .Include(i => i.EquipmentGroups)
+                    .ThenInclude(eg => eg.Equipment)
+                .Include(i => i.IntensityPreferences)
+                .Select(i => new ExerciseViewModel(null, i.Variation.Exercise, i.Variation, i)
+                {
+                    Verbosity = Verbosity.Diagnostic
+                })
+                .ToListAsync())
+                .OrderBy(vm => vm.Exercise.Name) // OrderBy must come after query or you get duplicates
+                .ThenBy(vm => vm.Intensity.Progression.Min)
+                .ThenBy(vm => vm.Intensity.Progression.Max == null)
+                .ThenBy(vm => vm.Intensity.Progression.Max);
+
+            var exercises = allExercises.Where(vm => vm.ActivityLevel == ExerciseActivityLevel.Main).ToList();
+            var viewModel = new NewsletterViewModel(exercises, Verbosity.Diagnostic)
             {
                 WarmupExercises = allExercises.Where(vm => vm.ActivityLevel == ExerciseActivityLevel.Warmup).ToList(),
                 CooldownExercises = allExercises.Where(vm => vm.ActivityLevel == ExerciseActivityLevel.Cooldown).ToList()
