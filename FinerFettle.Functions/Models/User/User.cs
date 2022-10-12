0edfc4e89ad0b25e3479e28d754bea43903cefa3
Graham Scanlon <gscanlon21@gmail.com>
2022-10-12 17:13:29 -0500

    feat: init azure functions, disable inactive accounts
    Former-commit-id: 5aa79dd237414c4a5f76b1908d9023809baae659

---
+++ FinerFettle.Functions/Models/User/User.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+
+namespace FinerFettle.Functions.Models.User
+{
+    /// <summary>
+    /// User who signed up for the newsletter.
+    /// </summary>
+    [DebuggerDisplay("Email = {Email}, Disabled = {Disabled}")]
+    public class User
+    {
+        public int Id { get; init; }
+
+        public string Email { get; set; } = null!;
+
+        public bool AcceptedTerms { get; set; }
+
+        public string? DisabledReason { get; set; } = null;
+
+        public bool Disabled => DisabledReason != null;
+
+        public DateOnly CreatedDate { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+
+        public DateOnly LastActive { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+
+        [InverseProperty(nameof(Newsletter.Newsletter.User))]
+        public virtual ICollection<Newsletter.Newsletter> Newsletters { get; set; } = default!;
+    }
+}
---
+++ FinerFettle.Functions/Startup.cs
@@ -0,0 +1,18 @@
+﻿using FinerFettle.Functions.Data;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(FinerFettle.Functions.Startup))]
+namespace FinerFettle.Functions
+{        
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddDbContext<CoreContext>(options =>
+                options.UseNpgsql(Environment.GetEnvironmentVariable("SqlConnectionString") ?? throw new InvalidOperationException("Connection string 'SqlConnectionString' not found.")));
+        }
+    }
+}
