7ee18b4a8ffbfdce8985af827b446f87b23faa04
Graham Scanlon <gscanlon21@gmail.com>
2022-10-13 07:53:23 -0500

    feat: show message to user when account is about to be deactivated for inactivity
    Former-commit-id: 034582a1188c1825742e8f865da774180f11255b

--- FinerFettle.Web/ViewModels/User/UserNewsletterViewModel.cs
+++ FinerFettle.Web/ViewModels/User/UserNewsletterViewModel.cs
@@ -3,6 +3,7 @@ using FinerFettle.Web.Models.Exercise;
 using FinerFettle.Web.Models.Newsletter;
 using FinerFettle.Web.Models.User;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FinerFettle.Web.ViewModels.User
 {
@@ -23,6 +24,7 @@ namespace FinerFettle.Web.ViewModels.User
             PrefersWeights = user.PrefersWeights;
             UserExercises = user.UserExercises;
             SportsFocus = user.SportsFocus;
+            LastActive = user.LastActive;
         }
 
         public MuscleGroups RecoveryMuscle { get; }
@@ -38,6 +40,10 @@ namespace FinerFettle.Web.ViewModels.User
         public ICollection<UserEquipment> UserEquipments { get; set; } = new List<UserEquipment>();
 
         public RestDays RestDays { get; set; } = RestDays.None;
+
+        public DateOnly LastActive { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+
+        public bool IsAlmostInactive => LastActive <= DateOnly.FromDateTime(DateTime.UtcNow).AddMonths(-11);
 
         public StrengtheningPreference StrengtheningPreference { get; set; } = StrengtheningPreference.Obtain;
 
