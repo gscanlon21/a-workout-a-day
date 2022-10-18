032535ee65b481e02092ac5987efa3b71b5fb6e2
Graham Scanlon <gscanlon21@gmail.com>
2022-10-18 18:08:14 -0500

    feat: log cleanup tasks
    Former-commit-id: 562039ad53a58cc6bedd339b625ae688b35be9c5

---
+++ FinerFettle.Functions/Models/User/UserToken.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinerFettle.Functions.Models.User
+{
+    /// <summary>
+    /// User's progression level of an exercise.
+    /// </summary>
+    [Table("user_token")]
+    public class UserToken
+    {
+        /// <summary>
+        /// Used as a unique user identifier in email links. This valus is switched out every day to expire old links.
+        /// 
+        /// This is kinda like a bearer token.
+        /// </summary>
+        public string Token { get; private set; } = null!;
+
+        public int UserId { get; set; }
+
+        public User User { get; set; } = null!;
+
+        /// <summary>
+        /// Unsubscribe links need to work for at least 60 days per law
+        /// </summary>
+        public DateOnly Expires { get; set; }
+    }
+}
