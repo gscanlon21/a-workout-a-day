0ae5923b103e544b5b4cf4a4be75eea755916bff
Graham Scanlon <gscanlon21@gmail.com>
2022-10-20 18:32:31 -0500

    feat: show targeted sports on the exercise debug page

--- FinerFettle.Web/Models/Newsletter/Verbosity.cs
+++ FinerFettle.Web/Models/Newsletter/Verbosity.cs
@@ -1,14 +1,28 @@
-﻿namespace FinerFettle.Web.Models.Newsletter
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinerFettle.Web.Models.Newsletter
 {
     /// <summary>
     /// The detail shown in the newsletter.
     /// </summary>
     public enum Verbosity
     {
+        [Display(Name = "Quiet")]
         Quiet = 1 << 0,
+
+        [Display(Name = "Minimal")]
         Minimal = 1 << 1 | Quiet,
+
+        [Display(Name = "Normal")]
         Normal = 1 << 2 | Minimal,
+
+        [Display(Name = "Detailed")]
         Detailed = 1 << 3 | Normal,
-        Diagnostic = 1 << 4 | Detailed
+
+        [Display(Name = "Diagnostic")]
+        Diagnostic = 1 << 4 | Detailed,
+
+        [Display(Name = null)]
+        Debug = 1 << 5 | Diagnostic
     }
 }
