17780b26e722a4ef66fd2632f79aa8b80bf09f7f
Graham Scanlon <gscanlon21@gmail.com>
2022-10-22 08:08:43 -0500

    feat: better data consistency checks

--- FinerFettle.Web/ViewModels/Exercise/CheckViewModel.cs
+++ FinerFettle.Web/ViewModels/Exercise/CheckViewModel.cs
@@ -7,6 +7,9 @@ namespace FinerFettle.Web.ViewModels.Exercise
     {
         public IList<string> Missing100PProgressionRange { get; init; } = null!;
         public IList<string> MissingRepRange { get; init; } = null!;
-        public IList<string> MissingProficiency { get; init; } = null!;
+        public IList<string> MissingProficiencyStrength { get; init; } = null!;
+        public IList<string> MissingProficiencyRecovery { get; init; } = null!;
+        public IList<string> MissingProficiencyWarmupCooldown { get; init; } = null!;
+        public IList<string> EmptyDisabledString { get; init; } = null!;
     }
 }
