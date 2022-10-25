21cc76129038b86328be5d0f760bf8865ae79873
Graham Scanlon <gscanlon21@gmail.com>
2022-10-25 17:48:37 -0500

    fix: bring back the endurance enum value

--- FinerFettle.Web/Models/Exercise/IntensityLevel.cs
+++ FinerFettle.Web/Models/Exercise/IntensityLevel.cs
@@ -8,7 +8,7 @@
         Maintain = 0,
         Obtain = 1,
         Gain = 2,
-        //Endurance = 3,
+        Endurance = 3,
         Recovery = 4,
         WarmupCooldown = 5 // Might split these out at some point 
     }
