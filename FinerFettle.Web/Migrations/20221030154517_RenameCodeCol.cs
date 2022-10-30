0a70472566e9fe71c2ed1a44d99b677e1144b9c4
Graham Scanlon <gscanlon21@gmail.com>
2022-10-30 10:50:16 -0500

    improvement: don't guess at image file extension
    Former-commit-id: 587178ebacb5713926e37f18c9bf9ea1b1722367

---
+++ FinerFettle.Web/Migrations/20221030154517_RenameCodeCol.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinerFettle.Web.Migrations
+{
+    public partial class RenameCodeCol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Code",
+                table: "variation",
+                newName: "ImageCode");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageCode",
+                table: "variation",
+                newName: "Code");
+        }
+    }
+}
