4560ab6e30cedf1059c4d2a158a0cf83a16cbf7c
Graham Scanlon <gscanlon21@gmail.com>
2022-11-13 09:41:38 -0600

    refactor: simplify query builder and enum functions

---
+++ Web/Data/QueryBuilder/WeightOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace Web.Data.QueryBuilder;
+
+public class WeightOptions
+{
+    public WeightOptions() { }
+
+    public WeightOptions(bool? prefersWeights)
+    {
+        PrefersWeights = prefersWeights;
+    }
+
+    /// <summary>
+    ///     If true, prefer weighted variations over bodyweight variations.
+    ///     If false, only show bodyweight variations.
+    ///     If null, show both weighted and bodyweight variations with equal precedence.
+    /// </summary>
+    public bool? PrefersWeights { get; private set; }
+
+    public bool OnlyWeights { get; set; }
+}
