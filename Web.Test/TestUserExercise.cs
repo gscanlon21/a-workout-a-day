d662d77c6ebc7310de0c0904bf298e731f53eac6
Graham Scanlon <gscanlon21@gmail.com>
2022-11-13 15:58:52 -0600

    test: user exercise default progression
    Former-commit-id: 1bf135a1f6d0a348db0dbad92a03bf491aa381ba

---
+++ Web.Test/TestUserExercise.cs
@@ -0,0 +1,18 @@
+﻿using Web.Entities.User;
+
+namespace Web.Test;
+
+[TestClass]
+public class TestUserExercise
+{
+    /// <summary>
+    /// Certain functionality relies on the default progression also being the MinUserProgression.
+    /// </summary>
+    [TestMethod]
+    public void UserExercise_Default_HasProgressionEqualMinProgression()
+    {
+        var userExercise = new UserExercise();
+
+        Assert.AreEqual(userExercise.Progression, UserExercise.MinUserProgression);
+    }
+}
