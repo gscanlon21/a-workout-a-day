703e4d6b3de7c181c57cce6a62f2f49c0f8d6613
Graham Scanlon <gscanlon21@gmail.com>
2022-11-12 19:06:13 -0600

    refactor: consolidate grabbing which muscles have been worked from a lsit of exercises

---
+++ FinerFettle.Web/Extensions/VariationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using FinerFettle.Web.Data;
+using FinerFettle.Web.Models.Exercise;
+
+namespace FinerFettle.Web.Extensions;
+
+public static class VariationExtensions
+{
+    public static MuscleGroups WorkedMuscles<T>(this IList<T> list, MuscleGroups? addition = null) where T : IExerciseVariationCombo
+    {
+        return list.Aggregate(addition ?? MuscleGroups.None, (acc, curr) => acc | curr.Variation.PrimaryMuscles);
+    }
+}
