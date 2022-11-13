5af535393b0d6a106113f7dbe3e0c9f57439c7d2
Graham Scanlon <gscanlon21@gmail.com>
2022-11-13 15:58:52 -0600

    test: user exercise default progression
    Former-commit-id: 6285bf0155399da93ac1c186550e979d373dc925

--- Web.Test/TestNewsletterTypeGroups.cs
+++ Web.Test/TestNewsletterTypeGroups.cs
@@ -3,34 +3,33 @@ using Web.Models.User;
 using Web.Models.Exercise;
 using Web.Extensions;
 
-namespace Web.Test
+namespace Web.Test;
+
+[TestClass]
+public class TestNewsletterTypeGroups
 {
-    [TestClass]
-    public class TestNewsletterTypeGroups
+    private static readonly MovementPattern AllMovementPatterns 
+        = MovementPattern.HorizontalPush 
+        | MovementPattern.HorizontalPull
+        | MovementPattern.VerticalPush
+        | MovementPattern.VerticalPull
+        | MovementPattern.HipHinge
+        | MovementPattern.Squat
+        | MovementPattern.Lunge
+        | MovementPattern.Carry
+        | MovementPattern.Rotation;
+
+    [TestMethod]
+    public void NewsletterTypeGroups_MaintainFullBody_HasAllMovementPatterns()
     {
-        private static readonly MovementPattern AllMovementPatterns 
-            = MovementPattern.HorizontalPush 
-            | MovementPattern.HorizontalPull
-            | MovementPattern.VerticalPush
-            | MovementPattern.VerticalPull
-            | MovementPattern.HipHinge
-            | MovementPattern.Squat
-            | MovementPattern.Lunge
-            | MovementPattern.Carry
-            | MovementPattern.Rotation;
+        var groups = new NewsletterTypeGroups(StrengtheningPreference.Maintain, Frequency.FullBody);
+        Assert.IsTrue(groups.Aggregate((MovementPattern)0, (curr, n) => curr | n.MovementPatterns).HasFlag(AllMovementPatterns));
+    }
 
-        [TestMethod]
-        public void NewsletterTypeGroups_MaintainFullBody_HasAllMovementPatterns()
-        {
-            var groups = new NewsletterTypeGroups(StrengtheningPreference.Maintain, Frequency.FullBody);
-            Assert.IsTrue(groups.Aggregate((MovementPattern)0, (curr, n) => curr | n.MovementPatterns).HasFlag(AllMovementPatterns));
-        }
-
-        [TestMethod]
-        public void NewsletterTypeGroups_MaintainUpperLower_HasAllMovementPatterns()
-        {
-            var groups = new NewsletterTypeGroups(StrengtheningPreference.Maintain, Frequency.UpperLowerBodySplit);
-            Assert.IsTrue(groups.Aggregate((MovementPattern)0, (curr, n) => curr | n.MovementPatterns).HasFlag(AllMovementPatterns));
-        }
+    [TestMethod]
+    public void NewsletterTypeGroups_MaintainUpperLower_HasAllMovementPatterns()
+    {
+        var groups = new NewsletterTypeGroups(StrengtheningPreference.Maintain, Frequency.UpperLowerBodySplit);
+        Assert.IsTrue(groups.Aggregate((MovementPattern)0, (curr, n) => curr | n.MovementPatterns).HasFlag(AllMovementPatterns));
     }
 }
