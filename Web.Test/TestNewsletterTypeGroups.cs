3d023b47860b20004904c5f29fff19d864551d42
Graham Scanlon <gscanlon21@gmail.com>
2022-11-16 20:13:56 -0600

    test: more tests for newsletter groups

--- Web.Test/TestNewsletterTypeGroups.cs
+++ Web.Test/TestNewsletterTypeGroups.cs
@@ -22,14 +22,28 @@ public class TestNewsletterTypeGroups
     [TestMethod]
     public void NewsletterTypeGroups_MaintainFullBody_HasAllMovementPatterns()
     {
-        var groups = new NewsletterTypeGroups(StrengtheningPreference.Maintain, Frequency.FullBody);
+        var groups = new NewsletterTypeGroups(StrengtheningPreference.Maintain, Frequency.FullBody2Day);
         Assert.IsTrue(groups.Aggregate((MovementPattern)0, (curr, n) => curr | n.MovementPatterns).HasFlag(AllMovementPatterns));
     }
 
     [TestMethod]
-    public void NewsletterTypeGroups_MaintainUpperLower_HasAllMovementPatterns()
+    public void NewsletterTypeGroups_MaintainUpperLower2Day_HasAllMovementPatterns()
     {
-        var groups = new NewsletterTypeGroups(StrengtheningPreference.Maintain, Frequency.UpperLowerBodySplit);
+        var groups = new NewsletterTypeGroups(StrengtheningPreference.Maintain, Frequency.UpperLowerBodySplit2Day);
+        Assert.IsTrue(groups.Aggregate((MovementPattern)0, (curr, n) => curr | n.MovementPatterns).HasFlag(AllMovementPatterns));
+    }
+
+    [TestMethod]
+    public void NewsletterTypeGroups_MaintainUpperLower4Day_HasAllMovementPatterns()
+    {
+        var groups = new NewsletterTypeGroups(StrengtheningPreference.Maintain, Frequency.UpperLowerBodySplit4Day);
+        Assert.IsTrue(groups.Aggregate((MovementPattern)0, (curr, n) => curr | n.MovementPatterns).HasFlag(AllMovementPatterns));
+    }
+
+    [TestMethod]
+    public void NewsletterTypeGroups_MaintainUpperPushPullLower_HasAllMovementPatterns()
+    {
+        var groups = new NewsletterTypeGroups(StrengtheningPreference.Maintain, Frequency.PushPullLeg3Day);
         Assert.IsTrue(groups.Aggregate((MovementPattern)0, (curr, n) => curr | n.MovementPatterns).HasFlag(AllMovementPatterns));
     }
 }
