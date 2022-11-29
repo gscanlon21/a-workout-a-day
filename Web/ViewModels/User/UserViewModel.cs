3d497286b28fd0c364954b71d1a1e4b50e3b6827
Graham Scanlon <gscanlon21@gmail.com>
2022-11-29 17:32:41 -0600

    feat: is-new-to-fitness and email-me-at-hour preferences

--- Web/ViewModels/User/UserViewModel.cs
+++ Web/ViewModels/User/UserViewModel.cs
@@ -32,11 +32,12 @@ public class UserViewModel
         RecoveryMuscle = user.RecoveryMuscle;
         DeloadAfterEveryXWeeks = user.DeloadAfterEveryXWeeks;
         IncludeBonus = user.IncludeBonus;
+        IsNewToFitness = user.IsNewToFitness;
         SportsFocus = user.SportsFocus;
         Token = token;
     }
 
-    public Entities.User.User User { get; }
+    public Entities.User.User? User { get; set; }
 
     /// <summary>
     /// If null, user has not yet tried to subscribe.
@@ -59,32 +60,40 @@ public class UserViewModel
     /// </summary>
     public bool? WasUpdated { get; set; }
 
+    [DataType(DataType.EmailAddress)]
     [Required, RegularExpression(@"\s*\S+@\S+\.\S+\s*", ErrorMessage = "Invalid email.")]
     [Remote(nameof(Controllers.UserValidationController.IsUserAvailable), Controllers.UserValidationController.Name, ErrorMessage = "Invalid email. Manage your preferences from the previous newsletter.")]
-    [DisplayName("Email"), DataType(DataType.EmailAddress)]
+    [Display(Name = "Email", Description = "We respect your privacy and sanity.")]
     public string Email { get; init; } = null!;
 
     public string? Token { get; init; }
 
     [Required, MustBeTrue]
+    [Display(Description = "You must be at least 13 years old.")]
     public bool AcceptedTerms { get; init; }
+
+    [Required]
+    [Display(Name = "I'm new to fitness", Description = "Simplifies the first few months of workouts to help make a habit out of working out.")]
+    public bool IsNewToFitness { get; init; } = true;
 
     /// <summary>
     /// Anti-bot honeypot
     /// </summary>
     [Required, MustBeTrue(DisableClientSideValidation = true, ErrorMessage = "Bot, I am your father.")]
+    [Display(Description = "If you're reading this, you're human.")]
     public bool IExist { get; init; }
 
     /// <summary>
     /// Pick weighted variations over calisthenics if available
     /// </summary>
-    [Required, DisplayName("Prefer Weights")]
+    [Required]
+    [Display(Name = "Prefer Weights", Description = "Prioritize exercises that use free weights over bodyweight variations.")]
     public bool PrefersWeights { get; init; }
 
     /// <summary>
     /// Don't strengthen this muscle group, but do show recovery variations for exercises
     /// </summary>
-    [DisplayName("Recovery Muscle (beta)")]
+    [Display(Name = "Recovery Muscle (beta)")]
     public MuscleGroups RecoveryMuscle { get; init; }
 
     public const int DeloadAfterEveryXWeeksMin = 2;
@@ -100,40 +109,44 @@ public class UserViewModel
     /// <summary>
     /// Include a section to boost a specific sports performance
     /// </summary>
-    [DisplayName("Sports Focus (beta)")]
+    [Display(Name = "Sports Focus (beta)", Description = "Include additional exercises that focus on the muscles and movements involved in a particular sport.")]
     public SportsFocus SportsFocus { get; init; }
 
-    [DisplayName("Disabled Reason")]
+    [Display(Name = "Disabled Reason")]
     public string? DisabledReason { get; init; }
 
-    [DisplayName("Disabled")]
+    [Display(Name = "Disabled", Description = "Stop receiving email without deleting your account.")]
     public bool Disabled { get; init; }
 
-    [DisplayName("Include Bonus Exercises")]
+    [Display(Name = "Include Bonus Exercises", Description = "Select this to add more exercise variety to your workout.")]
     public bool IncludeBonus { get; init; }
 
     [Required]
-    [DisplayName("Strengthening Preference")]
+    [Display(Name = "Strengthening Preference")]
     public StrengtheningPreference StrengtheningPreference { get; init; }
 
     [Required]
-    [DisplayName("Workout Split")]
+    [Display(Name = "Workout Split")]
     public Frequency Frequency { get; init; }
 
     [Required]
-    [DisplayName("Email Verbosity")]
+    [Display(Name = "Email Verbosity", Description = "Choose the level of detail you want to receive in each email.")]
     public Verbosity EmailVerbosity { get; init; }
 
+    [Required, Range(0, 23)]
+    [Display(Name = "Send Emails at this Hour (UTC)", Description = "What hour of the day (UTC) do you want to receive emails?")]
+    public int EmailAtUTCOffset { get; set; } = 0;
+
     [Required]
-    [DisplayName("Rest Days")]
+    [Display(Name = "Rest Days", Description = "Choose which days you want to take a break.")]
     public RestDays RestDays { get; private set; }
 
-    [DisplayName("Equipment")]
+    [Display(Name = "Equipment", Description = "Choose equipment you have access to each day.")]
     public IList<Equipment> Equipment { get; set; } = new List<Equipment>();
 
     public int[]? EquipmentBinder { get; init; }
 
-    [DisplayName("Ignored Exercises")]
+    [Display(Name = "Ignored Exercises", Description = "Choose exercises you want to ignore.")]
     public IList<Entities.Exercise.Exercise> IgnoredExercises { get; init; } = new List<Entities.Exercise.Exercise>();
 
     public int[]? IgnoredExerciseBinder { get; init; }
