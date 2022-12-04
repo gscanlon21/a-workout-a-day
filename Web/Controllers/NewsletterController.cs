a8945752db29ec8b1bb24bcd236e42dc40385e55
Graham Scanlon <gscanlon21@gmail.com>
2022-12-03 21:25:08 -0600

    fix: time until next deload calulaation

--- Web/Controllers/NewsletterController.cs
+++ Web/Controllers/NewsletterController.cs
@@ -91,35 +91,32 @@ public class NewsletterController : BaseController
     }
 
     /// <summary>
-    /// Checks if the user should deload for a week (reduce the intensity of their workout to reduce muscle growth stagnating).
+    /// Checks if the user should deload for a week.
+    /// 
+    /// Deloads are weeks with a message to lower the intensity of the workout so muscle growth doesn't stagnate.
+    /// Also to ease up the stress on joints.
     /// </summary>
     internal async Task<(bool needsDeload, TimeSpan timeUntilDeload)> CheckNewsletterDeloadStatus(User user)
     {
         var lastDeload = await _context.Newsletters
             .Where(n => n.User == user)
             .OrderBy(n => n.Date)
-            .ThenBy(n => n.Id) // For testing/demo. When two newsletters are sent the same day, I want a different exercise set.
-            .LastOrDefaultAsync(n => n.IsDeloadWeek)
-                ?? await _context.Newsletters
-                .Where(n => n.User == user)
-                .OrderBy(n => n.Date)
-                .ThenBy(n => n.Id) // For testing/demo. When two newsletters are sent the same day, I want a different exercise set.
-                .FirstOrDefaultAsync(); // The oldest newsletter, for if there has never been a deload before.
+            .LastOrDefaultAsync(n => n.IsDeloadWeek);
 
-        // Deloads are weeks with a message to lower the intensity of the workout so muscle growth doesn't stagnate.
-        // Also to ease up the stress on joints.
-        bool needsDeload = lastDeload != null
-            && (
-                // Dates are the same week. Keep the deload going until the week is over.
-                (lastDeload.IsDeloadWeek && lastDeload.Date.AddDays(-1 * (int)lastDeload.Date.DayOfWeek) == Today.AddDays(-1 * (int)Today.DayOfWeek))
-                // Or the last deload/oldest newsletter was 1+ months ago
-                || lastDeload.Date < Today.AddDays(-7 * user.DeloadAfterEveryXWeeks)
-            );
+        bool needsDeload = 
+            // Dates are the same week. Keep the deload going until the week is over.
+            (lastDeload != null && lastDeload.Date.AddDays(-1 * (int)lastDeload.Date.DayOfWeek) == Today.AddDays(-1 * (int)Today.DayOfWeek))
+            // Or the last deload was 1+ months ago.
+            || (lastDeload != null && lastDeload.Date < Today.AddDays(-7 * user.DeloadAfterEveryXWeeks))
+            // Or there has never been a deload before, look at the user's created date.
+            || (lastDeload == null && user.CreatedDate < Today.AddDays(-7 * user.DeloadAfterEveryXWeeks));
 
-        TimeSpan timeUntilDeload = lastDeload switch
+        TimeSpan timeUntilDeload = (needsDeload, lastDeload) switch
         {
-            null => TimeSpan.FromDays(user.DeloadAfterEveryXWeeks * 7),
-            not null when !lastDeload.IsDeloadWeek => TimeSpan.FromDays(lastDeload.Date.DayNumber - Today.AddDays(-7 * user.DeloadAfterEveryXWeeks).DayNumber),
+            // There's never been a deload before, calculate the next deload date using the user's created date.
+            (false, null) => TimeSpan.FromDays(user.CreatedDate.DayNumber - Today.AddDays(-7 * user.DeloadAfterEveryXWeeks).DayNumber),
+            // Calculate the next deload date using the last deload's date.
+            (false, not null) => TimeSpan.FromDays(lastDeload.Date.DayNumber - Today.AddDays(-7 * user.DeloadAfterEveryXWeeks).DayNumber),
             _ => TimeSpan.Zero
         };
 
