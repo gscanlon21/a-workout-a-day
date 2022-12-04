d96b27b302edbf98131e6c2e9cc9bcb7974f4313
Graham Scanlon <gscanlon21@gmail.com>
2022-12-04 13:18:40 -0600

    init
    Former-commit-id: a559a84358a7dcded83334283c353f1f5b71cded

---
+++ Web/Extensions/NameOfExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+
+namespace Web.Extensions;
+
+public static class NameOf<TSource>
+{
+    public static string Full(Expression<Func<TSource, object>> expression)
+    {
+        var memberExpression = expression.Body as MemberExpression;
+        if (memberExpression == null)
+        {
+            var unaryExpression = expression.Body as UnaryExpression;
+            if (unaryExpression != null && unaryExpression.NodeType == ExpressionType.Convert)
+                memberExpression = unaryExpression.Operand as MemberExpression;
+        }
+
+        var result = memberExpression.ToString();
+        result = result.Substring(result.IndexOf('.') + 1);
+
+        return result;
+    }
+
+    public static string Full(string sourceFieldName, Expression<Func<TSource, object>> expression)
+    {
+        var result = Full(expression);
+        result = string.IsNullOrEmpty(sourceFieldName) ? result : sourceFieldName + "." + result;
+        return result;
+    }
+}
