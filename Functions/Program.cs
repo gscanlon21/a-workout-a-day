306784404458980b004dbaa5b932d716d5030660
Graham Scanlon <gscanlon21@gmail.com>
2022-12-10 09:02:18 -0600

    chore: switch to .net7 isolated process for functions execution

---
+++ Functions/Program.cs
@@ -0,0 +1,17 @@
+﻿using FinerFettle.Functions.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(s =>
+    {
+        s.AddDbContext<CoreContext>(options =>
+            options.UseNpgsql(Environment.GetEnvironmentVariable("SqlConnectionString") ?? throw new InvalidOperationException("Connection string 'SqlConnectionString' not found.")));
+    })
+    .Build();
+
+host.Run();
+
