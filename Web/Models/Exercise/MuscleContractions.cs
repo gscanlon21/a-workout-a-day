695ec3e3d1f132c985c1607cc75e733d4ea3b834
Graham Scanlon <gscanlon21@gmail.com>
2023-01-01 09:26:37 -0600

    fix: missing display attributes
    Former-commit-id: f2a895e6abe17e273241f340857ed8341c2888a5

--- Web/Models/Exercise/MuscleContractions.cs
+++ Web/Models/Exercise/MuscleContractions.cs
@@ -1,4 +1,6 @@
-﻿namespace Web.Models.Exercise;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models.Exercise;
 
 /// <summary>
 /// Isometric/Concentric/Eccentric.
@@ -9,16 +11,19 @@ public enum MuscleContractions
     /// <summary>
     /// Isometric exercises involve constant muscle contraction without changing the actual length of your muscles.
     /// </summary>
+    [Display(Name = "Static")]
     Static = 1 << 0, // 1
 
     /// <summary>
     /// The muscle contracts and shortens. Pulling motion. 
     /// </summary>
+    [Display(Name = "Concentric")]
     Concentric = 1 << 1, // 2
 
     /// <summary>
     /// The muscle contracts and lengthens. Pushing motion.
     /// </summary>
+    [Display(Name = "Eccentric")]
     Eccentric = 1 << 2, // 4
 
     Dynamic = Concentric | Eccentric, // 6
--- Web/Models/Exercise/MuscleMovement.cs
+++ Web/Models/Exercise/MuscleMovement.cs
@@ -1,4 +1,6 @@
-﻿namespace Web.Models.Exercise;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models.Exercise;
 
 /// <summary>
 /// Isometric/Concentric/Eccentric.
@@ -9,20 +11,24 @@ public enum MuscleMovement
     /// <summary>
     /// Isometric exercises involve constant muscle contraction without changing the actual length of your muscles.
     /// </summary>
+    [Display(Name = "Isometric")] 
     Isometric = 1 << 0, // 1
 
     /// <summary>
     /// Isotonic exercises require a constant load or resistance while moving your muscles. Independent of the speed you do the exercise, you just need to focus on the resistance itself. 
     /// </summary>
+    [Display(Name = "Isotonic")]
     Isotonic = 1 << 1, // 2
 
     /// <summary>
     /// Plyometric exercises are all about exerting the maximum force in a short time, also called explosive training
     /// </summary>
+    [Display(Name = "Pylometric")]
     Pylometric = 1 << 2, // 4
 
     /// <summary>
     /// Isokinetic exercises are pretty much the opposite of Isotonic exercises. The resistance can change during the exercises, but you do them over a set period of time or a set number of exercises.
     /// </summary>
+    [Display(Name = "Isokinetic")]
     Isokinetic = 1 << 3, // 8
 }
