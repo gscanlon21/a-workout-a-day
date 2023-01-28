7998544a63201642e9bc7d525db640ddea82d87e
Graham Scanlon <gscanlon21@gmail.com>
2023-01-28 14:11:50 -0600

    improvement: make core less frequent if the other exercises work all the core muscles

--- Web/Code/Extensions/VariationExtensions.cs
+++ Web/Code/Extensions/VariationExtensions.cs
@@ -7,12 +7,12 @@ namespace Web.Code.Extensions;
 
 public static class VariationExtensions
 {
-    public static MuscleGroups WorkedMuscles<T>(this IList<T> list, Func<IExerciseVariationCombo, MuscleGroups> muscleTarget, MuscleGroups? addition = null) where T : IExerciseVariationCombo
+    public static MuscleGroups WorkedMuscles<T>(this IEnumerable<T> list, Func<IExerciseVariationCombo, MuscleGroups> muscleTarget, MuscleGroups? addition = null) where T : IExerciseVariationCombo
     {
         return list.Aggregate(addition ?? MuscleGroups.None, (acc, curr) => acc | muscleTarget(curr));
     }
 
-    public static IDictionary<MuscleGroups, int> WorkedMusclesDict<T>(this IList<T> list, Func<IExerciseVariationCombo, MuscleGroups> muscleTarget, IDictionary<MuscleGroups, int>? addition = null) where T : IExerciseVariationCombo
+    public static IDictionary<MuscleGroups, int> WorkedMusclesDict<T>(this IEnumerable<T> list, Func<IExerciseVariationCombo, MuscleGroups> muscleTarget, IDictionary<MuscleGroups, int>? addition = null) where T : IExerciseVariationCombo
     {
         return Enum.GetValues<MuscleGroups>().Where(e => BitOperations.PopCount((ulong)e) == 1).ToDictionary(k => k, v => (addition?[v] ?? 0) + list.Sum(r => muscleTarget(r).HasFlag(v) ? 1 : 0));
     }
