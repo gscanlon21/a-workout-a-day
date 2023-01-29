a17ed013834fd09285583db75cc0bed1e141af42
Graham Scanlon <gscanlon21@gmail.com>
2023-01-28 19:20:25 -0600

    fix: exclude muscle groups
    Former-commit-id: f2f08e31da491767907921567f2601db06baf67c

--- Web/Data/QueryBuilder/MuscleGroupOptions.cs
+++ Web/Data/QueryBuilder/MuscleGroupOptions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq.Expressions;
 using System.Numerics;
+using Web.Code.Extensions;
 using Web.Entities.Exercise;
 using Web.Models.Exercise;
 
@@ -24,13 +25,6 @@ public class MuscleGroupOptions
     public Expression<Func<IExerciseVariationCombo, MuscleGroups>> MuscleTarget { get; set; } = v => v.Variation.StrengthMuscles;
 
     public MuscleGroups MuscleGroups { get; } = MuscleGroups.All;
-
-    /// <summary>
-    ///     If null, does not exclude any muscle groups from the IncludeMuscle or MuscleGroups set.
-    ///     If MuscleGroups.None, does not exclude any muscle groups from the IncludeMuscle or MuscleGroups set.
-    ///     If > MuscleGroups.None, excludes these muscle groups from the IncludeMuscle or MuscleGroups set.
-    /// </summary>
-    public MuscleGroups? ExcludeMuscleGroups { get; set; }
 
     /// <summary>
     ///     If null, does not exclude any muscle groups from the IncludeMuscle or MuscleGroups set.
