3b4446848a587c1b64b240d8b670acd780f7f7f0
Graham Scanlon <gscanlon21@gmail.com>
2023-02-04 14:51:25 -0600

    init
    Former-commit-id: cdae8ad3402626d0cf176e6f100907ad1940124d

--- Web/Code/Extensions/DbContextExtensions.cs
+++ Web/Code/Extensions/DbContextExtensions.cs
@@ -9,4 +9,9 @@ public static class DbContextExtensions
         db.Set<T>().RemoveRange(currentItems.Except(newItems, getKey));
         db.Set<T>().AddRange(newItems.Except(currentItems, getKey));
     }
+
+    public static void AddMissing<T, TKey>(this DbContext db, IEnumerable<TKey> currentItems, IEnumerable<TKey> exercises, Func<TKey, T> newItem) where T : class
+    {
+        db.Set<T>().AddRange(exercises.ExceptBy(currentItems, a => a, b => b).Select(newItem));
+    }
 }
