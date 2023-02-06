b0b2ac2f1528cbaed632c056ae9ded897d6bc665
Graham Scanlon <gscanlon21@gmail.com>
2023-02-06 12:57:06 -0600

    style: misspelling of plyometrics

--- Web/Models/Exercise/IntensityLevel.cs
+++ Web/Models/Exercise/IntensityLevel.cs
@@ -9,28 +9,28 @@ public enum IntensityLevel
 {
     /// <summary>
     /// For Isotonic/Isokinetic exercises, 12-15 rep range.
-    /// For Isometric/Pylometric exercises, ~4x30s.
+    /// For Isometric/Plyometric exercises, ~4x30s.
     /// </summary>
     [Display(Name = "Endurance")]
     Endurance = 0,
 
     /// <summary>
     /// For Isotonic/Isokinetic exercises, 8-12 rep range.
-    /// For Isometric/Pylometric exercises, ~3x40s.
+    /// For Isometric/Plyometric exercises, ~3x40s.
     /// </summary>
     [Display(Name = "Hypertrophy")]
     Hypertrophy = 1,
 
     /// <summary>
     /// For Isotonic/Isokinetic exercises, 6-8 rep range.
-    /// For Isometric/Pylometric exercises, ~2x60s.
+    /// For Isometric/Plyometric exercises, ~2x60s.
     /// </summary>
     [Display(Name = "Strength")]
     Strength = 2,
 
     /// <summary>
     /// For Isotonic/Isokinetic exercises, 15-20 rep range.
-    /// For Isometric/Pylometric exercises, ~5x24s.
+    /// For Isometric/Plyometric exercises, ~5x24s.
     /// </summary>
     [Display(Name = "Stabilization")]
     Stabilization = 3,
