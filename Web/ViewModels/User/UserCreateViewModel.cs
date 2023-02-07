9c3a37acda378d6dac7bb652df6d96e1adc2d059
Graham Scanlon <gscanlon21@gmail.com>
2023-02-07 17:20:11 -0600

    wip: working on removing bootstrap
    Former-commit-id: 3e01b55205b37516b9cd24c3c055965bb895d644

--- Web/ViewModels/User/UserCreateViewModel.cs
+++ Web/ViewModels/User/UserCreateViewModel.cs
@@ -49,7 +49,7 @@ public class UserCreateViewModel
     public bool AcceptedTerms { get; init; }
 
     [Required]
-    [Display(Name = "I'm new to fitness", Description = "Simplifies workouts to help make working out a habit.")]
+    [Display(Name = "I'm new to fitness", Description = "Simplifies workouts to make working out a habit.")]
     public bool IsNewToFitness { get; init; }
 
     /// <summary>
