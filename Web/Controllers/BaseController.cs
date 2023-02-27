2999038dd6f6817fe68906ce6e51f6a5c5270feb
Graham Scanlon <gscanlon21@gmail.com>
2023-02-27 17:06:44 -0600

    refactor: app settings domain config
    Former-commit-id: 21adecc62dfcadee2eef4bb1226aabf8f5f28cc9

--- Web/Controllers/BaseController.cs
+++ Web/Controllers/BaseController.cs
@@ -8,12 +8,6 @@ public class BaseController : Controller
     protected readonly CoreContext _context;
 
     /// <summary>
-    /// Returns whether a given uri links to this site
-    /// </summary>
-    protected bool IsInternalDomain(Uri uri) => string.Equals(uri.Host, "finerfettle.com", StringComparison.OrdinalIgnoreCase)
-        || string.Equals(uri.Host, "finerfettle.azurewebsites.net", StringComparison.OrdinalIgnoreCase);
-
-    /// <summary>
     /// Today's date from UTC.
     /// </summary>
     protected static DateOnly Today => DateOnly.FromDateTime(DateTime.UtcNow);
