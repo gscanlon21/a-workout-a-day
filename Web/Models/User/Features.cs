86187a637857f78177be5168995cf29bbac50417
Graham Scanlon <gscanlon21@gmail.com>
2023-02-28 17:15:15 -0600

    refactor: enum order
    Former-commit-id: c2ba25a65067d1164695bc1b27d16adaf0adf21f

--- Web/Models/User/Features.cs
+++ Web/Models/User/Features.cs
@@ -7,18 +7,21 @@ public enum Features
 {
     None = 0,
 
-    [Display(Name = "Can View Demo Features")]
+    [Display(Name = "Is a Demo User")]
     Demo = 1 << 0, // 1
 
-    [Display(Name = "Can See Alpha Features")]
-    Alpha = 1 << 1, // 2
+    [Display(Name = "Is a Debug User")]
+    Debug = 1 << 1, // 2
 
-    [Display(Name = "Can See Beta Features")]
-    Beta = 1 << 2, // 4
-
-    [Display(Name = "Can View Debug Features")]
-    Debug = 1 << 3, // 8
+    [Display(Name = "Is a Test User")]
+    Test = 1 << 2, // 4
 
     [Display(Name = "Can Receive Multiple Emails Each Day")]
-    ManyEmails = 1 << 4, // 16
+    ManyEmails = 1 << 3, // 8
+
+    [Display(Name = "Can View Alpha Features")]
+    Alpha = 1 << 4, // 16
+
+    [Display(Name = "Can View Beta Features")]
+    Beta = 1 << 5, // 32
 }
