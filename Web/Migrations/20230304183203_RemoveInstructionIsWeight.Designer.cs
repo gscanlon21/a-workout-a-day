499c6a04c706844a98ae845e0a8f1a582fca53f7
Graham Scanlon <gscanlon21@gmail.com>
2023-03-04 13:40:40 -0600

    improvement: simplify isweighted filtering | show Pounds for weighted warmups

--- Web/Migrations/20230304183203_RemoveInstructionIsWeight.Designer.cs
+++ Web/Migrations/20230304183203_RemoveInstructionIsWeight.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Web.Data;
@@ -11,9 +12,11 @@ using Web.Data;
 namespace Web.Migrations
 {
     [DbContext(typeof(CoreContext))]
-    partial class CoreContextModelSnapshot : ModelSnapshot
+    [Migration("20230304183203_RemoveInstructionIsWeight")]
+    partial class RemoveInstructionIsWeight
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Web/Migrations/20230304183203_RemoveInstructionIsWeight.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveInstructionIsWeight : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsWeight",
+                table: "instruction");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWeight",
+                table: "instruction",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
