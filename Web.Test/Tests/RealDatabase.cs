61bb0dd7507eb4d5d2e912cbc73931dfafb2b2a0
Graham Scanlon <gscanlon21@gmail.com>
2023-03-18 14:25:26 -0500

    fix: test connection strings

--- Web.Test/Tests/RealDatabase.cs
+++ Web.Test/Tests/RealDatabase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Web.Data;
 
 namespace Web.Test.Tests;
@@ -8,7 +9,7 @@ public abstract class RealDatabase : FakeDatabase
     protected override CoreContext CreateContext()
     {
         var optionsBuilder = new DbContextOptionsBuilder<CoreContext>()
-            .UseNpgsql(Environment.GetEnvironmentVariable("SqlConnectionString"));
+            .UseNpgsql(Config.GetConnectionString("CoreContext"));
 
         return new CoreContext(optionsBuilder.Options);
     }
