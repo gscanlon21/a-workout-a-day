2575b1273e7c4e12b19e589faaa590406c468532
Graham Scanlon <gscanlon21@gmail.com>
2023-03-24 17:29:32 -0500

    refactor: shared consts for functions and web

--- Functions/Functions/NewsletterFunctions.cs
+++ Functions/Functions/NewsletterFunctions.cs
@@ -17,70 +17,20 @@ public class NewsletterFunctions
         _coreContext = coreContext;
     }
 
-    [Function("DisableNeverActiveUsers")]
-    public async Task DisableNeverActiveUsers([TimerTrigger(/*Daily*/ "0 0 0 * * *", RunOnStartup = Debug.RunOnStartup)] TimerInfo myTimer)
+    [Function("DeleteOldNewsletters")]
+    public async Task DeleteOldNewsletters([TimerTrigger(/*Daily*/ "0 0 0 * * *", RunOnStartup = Debug.RunOnStartup)] TimerInfo myTimer)
     {
-        const string disabledReason = "No account activity";
-
         var today = DateOnly.FromDateTime(DateTime.UtcNow);
-        var neverActive = await _coreContext.Users
-            .Where(u => u.DisabledReason == null)
-            // User has received an email
-            .Where(u => u.Newsletters.Count() > 0)
-            // User never confirmed their account
-            .Where(u => u.LastActive == null)
-            // Give the user 3 months to confirm their account
-            .Where(u => u.CreatedDate < today.AddMonths(-3))
+        var newslettersToRemove = await _coreContext.Newsletters
+            // Delete newsletter logs after 1 year
+            .Where(u => u.Date < today.AddYears(-1))
             .ToListAsync();
 
-        foreach (var user in neverActive)
+        foreach (var newsletter in newslettersToRemove)
         {
-            user.DisabledReason = disabledReason;
+            _coreContext.Remove(newsletter);
         }
 
         await _coreContext.SaveChangesAsync();
-    }
-
-    [Function("DisableInactiveUsers")]
-    public async Task DisableInactiveUsers([TimerTrigger(/*Weekly*/ "0 0 0 * * 0", RunOnStartup = Debug.RunOnStartup)] TimerInfo myTimer)
-    {
-        const string disabledReason = "No recent account activity";
-
-        var today = DateOnly.FromDateTime(DateTime.UtcNow);
-        var inactiveUsers = await _coreContext.Users
-            .Where(u => u.DisabledReason == null)
-            // User has no account activity in the past 6 months
-            .Where(u => (u.LastActive != null && u.LastActive < today.AddMonths(-6))
-                || (u.LastActive == null && u.CreatedDate < today.AddMonths(-6))
-            )
-            .ToListAsync();
-
-        foreach (var user in inactiveUsers)
-        {
-            user.DisabledReason = disabledReason;
-        }
-
-        await _coreContext.SaveChangesAsync();
-    }
-
-    [Function("DeleteInactiveUsers")]
-    public async Task DeleteInactiveUsers([TimerTrigger(/*Monthly*/ "0 0 0 1 * *", RunOnStartup = Debug.RunOnStartup)] TimerInfo myTimer)
-    {
-        var today = DateOnly.FromDateTime(DateTime.UtcNow);
-        var toDeleteUsers = await _coreContext.Users
-            // User is disabled
-            .Where(u => u.DisabledReason != null)
-            // User has not been active in the past year
-            .Where(u => (u.LastActive != null && u.LastActive < today.AddYears(-1))
-                || (u.LastActive == null && u.CreatedDate < today.AddYears(-1))
-            )
-            .ToListAsync();
-
-        foreach (var user in toDeleteUsers)
-        {
-            _coreContext.Users.Remove(user);
-        }
-
-        await _coreContext.SaveChangesAsync();
-    }
+    }    
 }
