0f17d9a6a410105276d7ad478df069cb43c8f754
Graham Scanlon <gscanlon21@gmail.com>
2023-03-25 10:24:04 -0500

    refactor: debug consts
    Former-commit-id: 735c261fc94f58e9a967d59b54e06cd1f975feba

---
+++ Core/Debug/Consts.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Core.Debug;
+
+/// <summary>
+/// Debug consts.
+/// </summary>
+public static class Consts
+{
+#if DEBUG
+    public const bool IsDebug = true;
+#else
+    public const bool IsDebug = false;
+#endif
+}
