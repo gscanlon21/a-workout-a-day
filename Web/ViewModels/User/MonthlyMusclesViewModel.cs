e6d26927a7eba38a3ed21c16dba5910caefef2d3
Graham Scanlon <git@tscanlon.com>
2023-04-28 12:51:05 -0500

    style: monthly muscle consts

--- Web/ViewModels/User/MonthlyMusclesViewModel.cs
+++ Web/ViewModels/User/MonthlyMusclesViewModel.cs
@@ -12,4 +12,40 @@ public class MonthlyMusclesViewModel
     public required IDictionary<MuscleGroups, int> WeeklyTimeUnderTension { get; set; }
 
     public required double WeeklyTimeUnderTensionAvg { get; set; }
+
+    /// <summary>
+    /// The avg minimum number of seconds per week each muscle group should be under tension.
+    /// </summary>
+    public const double MinSecsPerWeek = 200d;
+
+    /// <summary>
+    /// The avg maximum number of seconds per week each muscle group should be under tension.
+    /// </summary>
+    public const double MaxSecsPerWeek = 300d;
+
+    /// <summary>
+    /// The range of time under tension each musclegroup should be exposed to each week.
+    /// </summary>
+    public readonly IDictionary<MuscleGroups, Range> MuscleTargets = new Dictionary<MuscleGroups, Range>
+    {
+        [MuscleGroups.Glutes] = 250..500,
+        [MuscleGroups.Abdominals] = 250..500,
+        [MuscleGroups.Obliques] = 200..400,
+        [MuscleGroups.Pectorals] = 200..400,
+        [MuscleGroups.Deltoids] = 200..400,
+        [MuscleGroups.Hamstrings] = 200..400,
+        [MuscleGroups.Quadriceps] = 200..400,
+        [MuscleGroups.LatissimusDorsi] = 200..400,
+        [MuscleGroups.Trapezius] = 200..400,
+        [MuscleGroups.Rhomboids] = 175..350,
+        [MuscleGroups.ErectorSpinae] = 175..350,
+        [MuscleGroups.Forearms] = 150..300,
+        [MuscleGroups.Calves] = 150..300,
+        [MuscleGroups.HipFlexors] = 150..300,
+        [MuscleGroups.HipAdductors] = 150..300,
+        [MuscleGroups.Biceps] = 100..200,
+        [MuscleGroups.Triceps] = 100..200,
+        [MuscleGroups.SerratusAnterior] = 100..200,
+        [MuscleGroups.RotatorCuffs] = 100..200
+    };
 }
