b391a37886668db2c5935b6b011ee754374397e5
Graham Scanlon <git@tscanlon.com>
2023-05-02 17:25:54 -0500

    feat: show exercises and variations on the ignore variation and user edit views. Allow the user to refresh exercises to temporarily hide them from the workout.

--- Web/ViewModels/User/IgnoreVariationViewModel.cs
+++ Web/ViewModels/User/IgnoreVariationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Web.Entities.Exercise;
+using Web.Entities.User;
 using Web.ViewModels.Newsletter;
 
 namespace Web.ViewModels.User;
@@ -10,12 +11,22 @@ namespace Web.ViewModels.User;
 public class IgnoreVariationViewModel
 {
     [Display(Name = "Exercise", Description = "This will ignore the exercise and all of its variations.")]
-    public Entities.Exercise.Exercise? Exercise { get; init; }
+    public required Entities.Exercise.Exercise Exercise { get; init; }
 
-    [Display(Name = "Variation", Description = "This will just ignore the variation.")]
+    [Display(Name = "Variation", Description = "This will only ignore the variation.")]
     public required Variation Variation { get; init; }
+
+    [Display(Name = "Exercise Refreshes After", Description = "Temporarily remove these exercise variations from your workouts.")]
+    public required UserExercise UserExercise { get; init; }
+
+    [Display(Name = "Variation Refreshes After", Description = "Temporarily remove this variation from your workouts.")]
+    public required UserExerciseVariation UserExerciseVariation { get; init; }
 
     public bool? WasUpdated { get; init; }
 
-    public IList<ExerciseViewModel> ExerciseVariations { get; init; } = null!;
+    public required string Email { get; init; }
+    public required string Token { get; init; }
+
+    public IList<ExerciseViewModel> Exercises { get; init; } = null!;
+    public IList<ExerciseViewModel> Variations { get; init; } = null!;
 }
