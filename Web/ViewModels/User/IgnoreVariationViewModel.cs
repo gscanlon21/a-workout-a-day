600da6d3dcb57a8337aedee4333a711a0d8756fb
Graham Scanlon <git@tscanlon.com>
2023-05-05 17:24:56 -0500

    fix: grammar
    Former-commit-id: 632c2be800d5421fb6e97cf2c6b483016423cfd0

--- Web/ViewModels/User/IgnoreVariationViewModel.cs
+++ Web/ViewModels/User/IgnoreVariationViewModel.cs
@@ -13,7 +13,7 @@ public class IgnoreVariationViewModel
     [Display(Name = "Exercise", Description = "This will ignore the exercise and all of its variations.")]
     public required Entities.Exercise.Exercise Exercise { get; init; }
 
-    [Display(Name = "Variation", Description = "This will only ignore the variation.")]
+    [Display(Name = "Variation", Description = "This will ignore only the variation.")]
     public required Variation Variation { get; init; }
 
     [Display(Name = "Exercise Refreshes After", Description = "Temporarily remove these exercise variations from your workouts.")]
