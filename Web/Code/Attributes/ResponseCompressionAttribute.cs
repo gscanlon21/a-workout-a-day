2438c79e96cb2433de677f15fb7e85e8cfd7f625
Graham Scanlon <git@tscanlon.com>
2023-05-11 17:42:03 -0500

    fix: response compression while debugging

---
+++ Web/Code/Attributes/ResponseCompressionAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Code.Attributes;
+
+/// <summary>
+/// Toggle for enabling response compression for a specific route.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class ResponseCompressionAttribute : MiddlewareFilterAttribute
+{
+    public bool Enabled { get; set; } = true;
+
+    public ResponseCompressionAttribute() : base(typeof(ResponseCompressionAttribute)) { }
+
+    public void Configure(IApplicationBuilder applicationBuilder)
+    {
+        if (Enabled)
+        {
+            applicationBuilder.UseResponseCompression();
+        }
+    }
+}
