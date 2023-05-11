602ed1d502e901506170778fdfd4442bebbd52dd
Graham Scanlon <git@tscanlon.com>
2023-05-11 18:11:37 -0500

    fix: order of enum select options

--- Web/Code/Extensions/EnumExtensions.cs
+++ Web/Code/Extensions/EnumExtensions.cs
@@ -67,14 +67,20 @@ public static class EnumExtensions
             .ToArray();
     }
 
+    public enum EnumOrdering
+    {
+        Value = 0,
+        Text = 1 << 0 // 1
+    }
+
     /// <summary>
     /// Converts enum values to a select list for views
     /// </summary>
-    public static IList<SelectListItem> AsSelectListItems32<T>(this IEnumerable<T> values, T? defaultValue = null) where T : struct, Enum
+    public static IList<SelectListItem> AsSelectListItems32<T>(this IEnumerable<T> values, EnumOrdering order = EnumOrdering.Value, T? defaultValue = null) where T : struct, Enum
     {
         return values
             .OrderByDescending(v => Convert.ToInt32(v) == Convert.ToInt32(defaultValue))
-            .ThenBy(v => v.GetSingleDisplayName())
+            .ThenBy(v => order == EnumOrdering.Value ? Convert.ToInt32(defaultValue).ToString() : v.GetSingleDisplayName())
             .Select(v => new SelectListItem()
             {
                 Text = v.GetSingleDisplayName(),
