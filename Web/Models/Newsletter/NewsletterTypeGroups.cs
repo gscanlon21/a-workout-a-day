04d5d4ad035a723dbb094b33015e659381f36d4e
Graham Scanlon <git@tscanlon.com>
2023-05-19 19:56:59 -0500

    feat: add combination workout splits
    Former-commit-id: 04e9f2f4053db53c55b164134ab472395a479daa

--- Web/Models/Newsletter/NewsletterTypeGroups.cs
+++ Web/Models/Newsletter/NewsletterTypeGroups.cs
@@ -81,6 +81,75 @@ public class NewsletterTypeGroups : IEnumerable<NewsletterRotation>
             MovementPattern.VerticalPush | MovementPattern.VerticalPull | MovementPattern.Carry);
     }
 
+    /// <summary>
+    /// An implementation of the Full Body workout split.
+    /// </summary>
+    private static IEnumerator<NewsletterRotation> GetUpperLowerFullBody3DayRotation()
+    {
+        // Lower body first. The workouts are better w/o equipment, which tends to happen when someone signs up w/o selecting their equipment before the first workout arrives.
+        yield return new NewsletterRotation(1,
+            MuscleGroups.LowerBody,
+            MovementPattern.HipExtension | MovementPattern.Squat | MovementPattern.Lunge);
+
+        yield return new NewsletterRotation(2,
+            MuscleGroups.UpperBody,
+            MovementPattern.HorizontalPush | MovementPattern.HorizontalPull | MovementPattern.Rotation);
+
+        yield return new NewsletterRotation(3,
+            MuscleGroups.UpperLower,
+            MovementPattern.VerticalPush | MovementPattern.VerticalPull | MovementPattern.KneeFlexion | MovementPattern.HipExtension | MovementPattern.Carry);
+    }
+
+    /// <summary>
+    /// An implementation of the Full Body workout split.
+    /// </summary>
+    private static IEnumerator<NewsletterRotation> GetPushPullLegsFullBody4DayRotation()
+    {
+        // Lower body first. The workouts are better w/o equipment, which tends to happen when someone signs up w/o selecting their equipment before the first workout arrives.
+        yield return new NewsletterRotation(1,
+            MuscleGroups.LowerBody,
+            MovementPattern.HipExtension | MovementPattern.Squat | MovementPattern.Lunge);
+
+        yield return new NewsletterRotation(2,
+            MuscleGroups.UpperBodyPush,
+            MovementPattern.HorizontalPush | MovementPattern.VerticalPush | MovementPattern.Carry);
+
+        yield return new NewsletterRotation(3,
+            MuscleGroups.UpperBodyPull,
+            MovementPattern.HorizontalPull | MovementPattern.VerticalPull | MovementPattern.Rotation);
+
+        yield return new NewsletterRotation(4,
+            MuscleGroups.UpperLower,
+            MovementPattern.Squat | MovementPattern.Lunge | MovementPattern.HipExtension);
+    }
+
+    /// <summary>
+    /// An implementation of the Full Body workout split.
+    /// </summary>
+    private static IEnumerator<NewsletterRotation> GetPushPullLegsUpperLower5DayRotation()
+    {
+        // Lower body first. The workouts are better w/o equipment, which tends to happen when someone signs up w/o selecting their equipment before the first workout arrives.
+        yield return new NewsletterRotation(1,
+            MuscleGroups.LowerBody,
+            MovementPattern.HipExtension | MovementPattern.Squat | MovementPattern.Lunge);
+
+        yield return new NewsletterRotation(2,
+            MuscleGroups.UpperBodyPush,
+            MovementPattern.HorizontalPush | MovementPattern.VerticalPush | MovementPattern.Carry);
+
+        yield return new NewsletterRotation(3,
+            MuscleGroups.UpperBodyPull,
+            MovementPattern.HorizontalPull | MovementPattern.VerticalPull | MovementPattern.Rotation);
+
+        yield return new NewsletterRotation(4,
+            MuscleGroups.LowerBody,
+            MovementPattern.HipExtension | MovementPattern.Squat | MovementPattern.Lunge);
+
+        yield return new NewsletterRotation(5,
+            MuscleGroups.UpperBody,
+            MovementPattern.Carry | MovementPattern.Rotation);
+    }
+
     public IEnumerator<NewsletterRotation> GetEnumerator()
     {
         return Frequency switch
@@ -88,6 +157,9 @@ public class NewsletterTypeGroups : IEnumerable<NewsletterRotation>
             Frequency.FullBody2Day => GetFullBody2DayRotation(),
             Frequency.PushPullLeg3Day => GetPushPullLeg3DayRotation(),
             Frequency.UpperLowerBodySplit4Day => GetUpperLower4DayRotation(),
+            Frequency.UpperLowerFullBodySplit3Day => GetUpperLowerFullBody3DayRotation(),
+            Frequency.PushPullLegsFullBodySplit4Day => GetPushPullLegsFullBody4DayRotation(),
+            Frequency.PushPullLegsUpperLowerSplit5Day => GetPushPullLegsUpperLower5DayRotation(),
             Frequency.OffDayStretches => GetOffDayStretchingRotation(),
             _ => throw new NotImplementedException()
         };
