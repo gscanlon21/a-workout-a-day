8b61e359bb683e458d7a2cc5cb04f5d3e11a5ea1
Graham Scanlon <git@tscanlon.com>
2023-05-24 17:50:59 -0500

    refactor: adding userexercise records
    Former-commit-id: 8d76409f90e6130b12230a9ad7f6d731bd4c4d39

--- Web/Code/Extensions/DbContextExtensions.cs
+++ Web/Code/Extensions/DbContextExtensions.cs
@@ -12,20 +12,4 @@ public static class DbContextExtensions
         db.Set<T>().RemoveRange(currentItems.Except(newItems, getKey, getKey));
         db.Set<T>().AddRange(newItems.Except(currentItems, getKey, getKey));
     }
-
-    /// <summary>
-    /// Adds required many-many records into the db that are not already there.
-    /// </summary>
-    public static void AddMissing<T, O, TKey>(this DbContext db, IEnumerable<TKey> currentItems, IEnumerable<O> allItems, Func<O, TKey> keySelector, Func<O, T> newItem) where T : class
-    {
-        db.Set<T>().AddRange(allItems.Except(currentItems, keySelector, a => a).Select(newItem).Distinct());
-    }
-
-    /// <summary>
-    /// Adds required many-many records into the db that are not already there.
-    /// </summary>
-    public static void AddMissing<T, TKey>(this DbContext db, IEnumerable<TKey> currentItems, IEnumerable<TKey> allItems, Func<TKey, T> newItem) where T : class
-    {
-        db.Set<T>().AddRange(allItems.Except(currentItems, a => a, a => a).Select(newItem).Distinct());
-    }
 }
