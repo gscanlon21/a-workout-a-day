32aa187d3f5cb75c2006f0d20f0e6aaed2de5232
Graham Scanlon <git@tscanlon.com>
2023-05-28 13:10:44 -0500

    feat: positive affirmations for mental health
    Former-commit-id: a5965696fde27fe8a8e0b3c9d5531cec41c63691

---
+++ Web/Migrations/20230528170107_AddingFootnoteType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingFootnoteType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "footnote",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "footnote");
+        }
+    }
+}
