80c3da6c06177f09878ecd41c7b58aac960a51be
Graham Scanlon <git@tscanlon.com>
2023-06-01 23:57:21 -0500

    docs: add summary comments
    Former-commit-id: 2d0e2230c77beaf0522c16934069218e0215a7fd

--- Web/Models/Footnote/FootnoteType.cs
+++ Web/Models/Footnote/FootnoteType.cs
@@ -8,49 +8,69 @@ public enum FootnoteType
     None = 0,
 
     /// <summary>
-    /// Take five to 10 minutes to warm up and cool down properly.
+    /// Fitness advice and tips. 
+    /// 
+    /// sa. Take five to 10 minutes to warm up and cool down properly.
     /// </summary>
     [Display(Name = "Fitness Advice", Description = "sa. Take five to 10 minutes to warm up and cool down properly.")]
     FitnessAdvice = 1 << 0, // 1
 
     /// <summary>
-    /// Practicing everyday mindfulness can improve your memory and concentration skills....
+    /// Life advice and tips.
+    /// 
+    /// sa. Practicing everyday mindfulness can improve your memory and concentration skills....
     /// </summary>
     [Display(Name = "Life Advice", Description = "sa. Practicing everyday mindfulness can improve your memory and concentration skills...")]
     LifeAdvice = 1 << 1, // 2
 
     /// <summary>
-    /// Never give up!
+    /// Fitness motivation.
+    /// 
+    /// sa. Never give up!
     /// </summary>
     [Display(Name = "Fitness Motivation", Description = "sa. Never give up!")]
     FitnessMotivation = 1 << 2, // 4
 
     /// <summary>
-    /// Never give up!
+    /// Life motivation.
+    /// 
+    /// sa. Never give up!
     /// </summary>
     [Display(Name = "Life Motivation", Description = "sa. Never give up!")]
     LifeMotivation = 1 << 3, // 8
 
     /// <summary>
-    /// I'm getting stronger after every workout
+    /// Fitness affmirmations. 
+    /// 
+    /// sa. I'm getting stronger after every workout
     /// </summary>
     [Display(Name = "Fitness Affirmations", Description = "sa. I'm getting stronger after every workout.")]
     FitnessAffirmations = 1 << 4, // 16
 
     /// <summary>
-    /// I'm a thoughtful and interesting person.
+    /// Life affirmations. 
+    /// 
+    /// sa. I'm a thoughtful and interesting person.
     /// </summary>
     [Display(Name = "Life Affirmations", Description = "sa. I'm a thoughtful and interesting person.")]
     LifeAffirmations = 1 << 5, // 32
 
     /// <summary>
-    /// You are beautiful!
+    /// Good vibes.
+    /// 
+    /// sa. You are beautiful!
     /// </summary>
     [Display(Name = "Good Vibes", Description = "sa. You are beautiful!")]
     GoodVibes = 1 << 6, // 64
 
+    /// <summary>
+    /// Footnotes to show above the workout.
+    /// </summary>
     Top = All & ~Bottom, // FitnessAffirmations | LifeAffirmations,
 
+    /// <summary>
+    /// Footnotes to show below the workout.
+    /// </summary>
     Bottom = FitnessAdvice | FitnessMotivation | LifeAdvice | LifeMotivation | GoodVibes,
 
     All = FitnessAdvice | FitnessMotivation | FitnessAffirmations | LifeAdvice | LifeMotivation | LifeAffirmations | GoodVibes
