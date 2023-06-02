ba009dec76f472e55bea683fcbccdc7a86f3d376
Graham Scanlon <git@tscanlon.com>
2023-06-01 19:08:56 -0500

    improvement: debug footnote data
    Former-commit-id: f7c53a44d782478887ebe1d1f155298e77082be8

---
+++ Web/ViewModels/Newsletter/FootnoteViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Web.Entities.Footnote;
+using Web.ViewModels.User;
+
+namespace Web.ViewModels.Newsletter;
+
+public class FootnoteViewModel
+{
+    public UserNewsletterViewModel User { get; init; } = null!;
+    public IList<Footnote> Footnotes { get; init; } = null!;
+}
