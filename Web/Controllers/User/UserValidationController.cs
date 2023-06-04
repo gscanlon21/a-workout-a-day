28c7015ce45f5d6485940b0fa96c98d69e57f3b3
Graham Scanlon <git@tscanlon.com>
2023-06-03 20:22:55 -0500

    feat: show a warning before allowing gmail addresses to sign up
    Former-commit-id: f7a7d71a7a2d3d0c9c950c50e561d0d3ef02afd6

--- Web/Controllers/User/UserValidationController.cs
+++ Web/Controllers/User/UserValidationController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Web.Data;
+using Web.Models.Options;
 
 namespace Web.Controllers.User;
 
@@ -13,7 +15,14 @@ public class UserValidationController : BaseController
     /// </summary>
     public const string Name = "UserValidation";
 
-    public UserValidationController(CoreContext context) : base(context) { }
+    private readonly CoreContext _context;
+    private readonly IOptions<SiteSettings> _siteSettings;
+
+    public UserValidationController(CoreContext context, IOptions<SiteSettings> siteSettings) : base()
+    {
+        _context = context;
+        _siteSettings = siteSettings;
+    }
 
     /// <summary>
     /// Validation route for whether a user already exists in the database
@@ -21,6 +30,25 @@ public class UserValidationController : BaseController
     [AllowAnonymous, Route("email")]
     public async Task<JsonResult> IsUserAvailable(string email)
     {
-        return Json(!await _context.Users.AnyAsync(u => EF.Functions.ILike(u.Email, email.Trim())));
+        email = email.Trim();
+
+        // Gmail does not support position:absolute.
+        // https://www.caniemail.com/search/?s=absolute 
+        var validGmailEnding = $"+{_siteSettings.Value.ApexDomainSansTLD}@gmail.com";
+        if (email.EndsWith("@gmail.com", StringComparison.OrdinalIgnoreCase)
+            && !email.EndsWith(validGmailEnding, StringComparison.OrdinalIgnoreCase))
+        {
+            var splitEmail = email.Split('+', '@');
+            // Fragmented; muddled; diorganized; disjointed; jumbled.
+            return Json($"Gmail is not a supported email client. Emails may appear garbled. If you understand, use the email: {splitEmail[0] + validGmailEnding}");
+        }
+
+        // The same user is already signed up.
+        if (await _context.Users.AnyAsync(u => EF.Functions.ILike(u.Email, email)))
+        {
+            return Json("Manage your preferences from the previous newsletter.");
+        }
+
+        return Json(true);
     }
 }
