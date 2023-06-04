a5be6a96dd140bb04f24d3df0829baea2113b614
Graham Scanlon <git@tscanlon.com>
2023-06-03 21:39:33 -0500

    improvement: run functions daily
    Former-commit-id: c874fe9523329d7c0f708a819ddc90a5acc069b4

--- Functions/Functions/UserFunctions.cs
+++ Functions/Functions/UserFunctions.cs
@@ -9,6 +9,8 @@ namespace FinerFettle.Functions.Functions;
 
 public class UserFunctions
 {
+    private static DateOnly Today => DateOnly.FromDateTime(DateTime.UtcNow);
+
     private readonly CoreContext _coreContext;
 
     public UserFunctions(CoreContext coreContext)
@@ -16,17 +18,19 @@ public class UserFunctions
         _coreContext = coreContext;
     }
 
+    /// <summary>
+    /// Disable inactive users.
+    /// </summary>
     [Function(nameof(DisableInactiveUsers))]
-    public async Task DisableInactiveUsers([TimerTrigger(/*Weekly*/ "0 0 0 * * 0", RunOnStartup = Core.Debug.Consts.IsDebug)] TimerInfo timerInfo)
+    public async Task DisableInactiveUsers([TimerTrigger(/*Daily*/ "0 0 0 * * *", RunOnStartup = Core.Debug.Consts.IsDebug)] TimerInfo timerInfo)
     {
         const string disabledReason = "No recent account activity";
 
-        var today = DateOnly.FromDateTime(DateTime.UtcNow);
         var inactiveUsers = await _coreContext.Users
             .Where(u => u.DisabledReason == null)
             // User has no account activity in the past X months
-            .Where(u => (u.LastActive.HasValue && u.LastActive.Value < today.AddMonths(-1 * Core.User.Consts.DisableAfterXMonths))
-                || (!u.LastActive.HasValue && u.CreatedDate < today.AddMonths(-1 * Core.User.Consts.DisableAfterXMonths))
+            .Where(u => (u.LastActive.HasValue && u.LastActive.Value < Today.AddMonths(-1 * Core.User.Consts.DisableAfterXMonths))
+                || (!u.LastActive.HasValue && u.CreatedDate < Today.AddMonths(-1 * Core.User.Consts.DisableAfterXMonths))
             )
             .ToListAsync();
 
@@ -38,33 +42,29 @@ public class UserFunctions
         await _coreContext.SaveChangesAsync();
     }
 
+    /// <summary>
+    /// Delete inactive users.
+    /// </summary>
     [Function(nameof(DeleteInactiveUsers))]
-    public async Task DeleteInactiveUsers([TimerTrigger(/*Monthly*/ "0 0 0 1 * *", RunOnStartup = Core.Debug.Consts.IsDebug)] TimerInfo timerInfo)
+    public async Task DeleteInactiveUsers([TimerTrigger(/*Daily*/ "0 0 0 * * *", RunOnStartup = Core.Debug.Consts.IsDebug)] TimerInfo timerInfo)
     {
-        var today = DateOnly.FromDateTime(DateTime.UtcNow);
-        var toDeleteUsers = await _coreContext.Users
+        await _coreContext.Users
             // User is disabled
             .Where(u => u.DisabledReason != null)
             // User has not been active in the past X months
-            .Where(u => (u.LastActive != null && u.LastActive < today.AddMonths(-1 * Core.User.Consts.DeleteAfterXMonths))
-                || (u.LastActive == null && u.CreatedDate < today.AddMonths(-1 * Core.User.Consts.DeleteAfterXMonths))
-            )
-            .ToListAsync();
-
-        _coreContext.Users.RemoveRange(toDeleteUsers);
-        await _coreContext.SaveChangesAsync();
+            .Where(u => (u.LastActive != null && u.LastActive < Today.AddMonths(-1 * Core.User.Consts.DeleteAfterXMonths))
+                || (u.LastActive == null && u.CreatedDate < Today.AddMonths(-1 * Core.User.Consts.DeleteAfterXMonths))
+            ).ExecuteDeleteAsync();
     }
 
+    /// <summary>
+    /// Delete expired tokens after 1 day.
+    /// </summary>
     [Function(nameof(DeleteOldTokens))]
     public async Task DeleteOldTokens([TimerTrigger(/*Daily*/ "0 0 0 * * *", RunOnStartup = Core.Debug.Consts.IsDebug)] TimerInfo timerInfo)
     {
-        var today = DateOnly.FromDateTime(DateTime.UtcNow);
-        var userTokensToRemove = await _coreContext.UserTokens
-            // Delete expired tokens after 1 day
-            .Where(u => u.Expires < today.AddDays(-1))
-            .ToListAsync();
-
-        _coreContext.UserTokens.RemoveRange(userTokensToRemove);
-        await _coreContext.SaveChangesAsync();
+        await _coreContext.UserTokens
+            .Where(u => u.Expires < Today.AddDays(-1))
+            .ExecuteDeleteAsync();
     }
 }
