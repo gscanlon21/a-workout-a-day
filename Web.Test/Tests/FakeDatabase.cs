d568b10971ac605edf9f192a1995cbea29152f88
Graham Scanlon <git@tscanlon.com>
2023-06-06 17:32:08 -0500

    chore: github actions
    Former-commit-id: 948a4c8b99b06a8a6d865fdb46c114535b89c84c

--- Web.Test/Tests/FakeDatabase.cs
+++ Web.Test/Tests/FakeDatabase.cs
@@ -23,7 +23,7 @@ public abstract class FakeDatabase
     {
         Config = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.test.json")
+            .AddJsonFile("appsettings.test.json", optional: true)
             .AddEnvironmentVariables()
             .Build();
 
