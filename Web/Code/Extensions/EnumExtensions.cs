a21f6fb878839e527f176c875986c65721dfdb0f
Graham <github@tscanlon.com>
2023-06-11 10:36:31 -0500

    Update README.md
    Former-commit-id: 0eb627a167f5f87ea406005475a74e4e2f53179d

--- Web/Code/Extensions/EnumExtensions.cs
+++ Web/Code/Extensions/EnumExtensions.cs
@@ -82,6 +82,18 @@ public static class EnumExtensions
     /// <summary>
     /// Returns enum values where the value has 1 or more bits set
     /// </summary>
+    public static T[] GetSingleValuesExcluding32<T>(params T[] excludes) where T : struct, Enum
+    {
+        var excludeValues = excludes.Select(exclude => Convert.ToInt32(exclude));
+        return Enum.GetValues<T>()
+            .Where(e => BitOperations.PopCount((ulong)Convert.ToInt32(e)) == 1)
+            .Where(e => !excludeValues.Contains(Convert.ToInt32(e)))
+            .ToArray();
+    }
+
+    /// <summary>
+    /// Returns enum values where the value has 1 or more bits set
+    /// </summary>
     public static T[] GetNotNoneValues32<T>() where T : struct, Enum
     {
         return Enum.GetValues<T>()
