376531a0ea81162e5a04e8017983b67b7e3b418a
Graham Scanlon <git@tscanlon.com>
2023-06-17 22:07:07 -0500

    init: shared files project
    Former-commit-id: 30d15f7e232c3059a45f41a6c12034d4de5d28f7

---
+++ Shared/NewsletterViewModel.cs
@@ -0,0 +1,37 @@
+using Web.Models.Newsletter;
+using Web.ViewModels.User;
+
+namespace Shared.ViewModels.Newsletter;
+
+/// <summary>
+/// Viewmodel for Newsletter.cshtml
+/// </summary>
+public class NewsletterViewModel
+{
+    /// <summary>
+    /// The number of footnotes to show in the newsletter
+    /// </summary>
+    public readonly int FootnoteCount = 2;
+
+    public NewsletterViewModel(UserNewsletterViewModel user, Entities.Newsletter.Newsletter newsletter)
+    {
+        User = user;
+        Newsletter = newsletter;
+        Verbosity = user.EmailVerbosity;
+    }
+
+    public UserNewsletterViewModel User { get; }
+    public Entities.Newsletter.Newsletter Newsletter { get; }
+
+    /// <summary>
+    /// How much detail to show in the newsletter.
+    /// </summary>
+    public Verbosity Verbosity { get; }
+
+    public required IList<ExerciseViewModel> MainExercises { get; init; } = null!;
+    public required IList<ExerciseViewModel> PrehabExercises { get; init; } = null!;
+    public required IList<ExerciseViewModel> RehabExercises { get; init; } = null!;
+    public required IList<ExerciseViewModel> WarmupExercises { get; init; } = null!;
+    public required IList<ExerciseViewModel> SportsExercises { get; init; } = null!;
+    public required IList<ExerciseViewModel> CooldownExercises { get; init; } = null!;
+}
