b9bd690d72b37cea79283a864737f614b9a8ec54
Graham Scanlon <git@tscanlon.com>
2023-06-17 21:56:58 -0500

    init: native app
    Former-commit-id: 0c391775b79ab2d7711f6a5b82a7d1df5f0d0006

---
+++ Native/Services/RestService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Mobile.Services;
+
+public class RestService 
+{
+    HttpClient _client;
+    JsonSerializerOptions _serializerOptions;
+
+    public List<object> Items { get; private set; }
+
+    public RestService()
+    {
+        _client = new HttpClient();
+        _serializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+    }
+
+    public async Task<List<object>> RefreshDataAsync()
+    {
+        Items = new List<object>();
+
+        Uri uri = new Uri(string.Format("", string.Empty));
+        try
+        {
+            HttpResponseMessage response = await _client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                Items = JsonSerializer.Deserialize<List<object>>(content, _serializerOptions);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(@"\tERROR {0}", ex.Message);
+        }
+
+        return Items;
+    }
+}
