0b48afc1fcc2269adc5c8bbe110afe017ad94484
Graham Scanlon <git@tscanlon.com>
2023-07-01 10:38:47 -0500

    improvement: azure logging and mail retry handling

---
+++ Api/Mail/Azure/Catch429Policy.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Core.Pipeline;
+using Azure.Core;
+using Azure;
+
+namespace Api.Mail.Azure;
+
+/// <summary>
+/// Throws an exception when email sending tier limit is reached.
+/// So Azure doesn't automatically retry the messages, we handle that.
+/// 
+/// https://learn.microsoft.com/en-us/azure/communication-services/quickstarts/email/send-email-advanced/throw-exception-when-tier-limit-reached?pivots=programming-language-csharp
+/// </summary>
+public class Catch429Policy : HttpPipelineSynchronousPolicy
+{
+    public override void OnReceivedResponse(HttpMessage message)
+    {
+        if (message.Response.Status == 429)
+        {
+            throw new RequestFailedException(message.Response);
+        }
+        else
+        {
+            base.OnReceivedResponse(message);
+        }
+    }
+}
