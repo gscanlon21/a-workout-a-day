eb7080de120d728634561a76f1a6b1d3719b1cd5
Graham Scanlon <git@tscanlon.com>
2023-07-14 18:33:42 -0500

    improvement: newsletter consts

---
+++ Core/Consts/NewsletterConsts.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Core.Consts;
+
+/// <summary>
+/// Shared user consts for Functions and Web.
+/// </summary>
+public class NewsletterConsts
+{
+    public const string SubjectLogin = "Login to Your Account";
+
+    public const string SubjectConfirm = "Confirm Your Account";
+
+    public const string SubjectWorkout = "Daily Workout";
+}
