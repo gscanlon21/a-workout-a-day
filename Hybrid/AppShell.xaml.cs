4c1fc57943dde0ba59caae499f9cfdc8f321a267
Graham Scanlon <git@tscanlon.com>
2023-07-14 18:36:05 -0500

    improvement: app flyout menus
    Former-commit-id: ff15afe416cb8c2d7d6ebed17dc8a21f8cf555ce

--- Hybrid/AppShell.xaml.cs
+++ Hybrid/AppShell.xaml.cs
@@ -3,47 +3,45 @@ using Core.Models.Options;
 using Microsoft.Extensions.Options;
 using System.Windows.Input;
 
-namespace Hybrid
+namespace Hybrid;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    /// <param name="serviceProvider">https://github.com/dotnet/maui/issues/11485</param>
+    public AppShell(IServiceProvider serviceProvider)
     {
-        /// <param name="serviceProvider">https://github.com/dotnet/maui/issues/11485</param>
-        public AppShell(IServiceProvider serviceProvider)
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            //Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
-            //Routing.RegisterRoute(nameof(NewsletterPage), typeof(NewsletterPage));
+        //Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+        //Routing.RegisterRoute(nameof(NewsletterPage), typeof(NewsletterPage));
 
-            BindingContext = serviceProvider.GetRequiredService<AppShellViewModel>();
-        }
+        BindingContext = serviceProvider.GetRequiredService<AppShellViewModel>();
     }
+}
 
-    public class AppShellViewModel
+public class AppShellViewModel
+{
+    public IOptions<SiteSettings> SiteSettings { get; set; }
+
+    public ICommand BrowserCommand { private set; get; }
+    public ICommand LogoutCommand { private set; get; }
+
+    public AppShellViewModel(IOptions<SiteSettings> siteSettings, IServiceProvider serviceProvider)
     {
-        public IOptions<SiteSettings> SiteSettings { get; set; }
-
-        public ICommand SourceCommand { private set; get; }
-        public ICommand LogoutCommand { private set; get; }
-
-        public AppShellViewModel(IOptions<SiteSettings> siteSettings, IServiceProvider serviceProvider)
+        SiteSettings = siteSettings;
+        BrowserCommand = new Command<string>(async (string arg) =>
         {
-            SiteSettings = siteSettings;
-            SourceCommand = new Command<string>(async (string arg) =>
-            {
-                await Browser.Default.OpenAsync(arg, BrowserLaunchMode.SystemPreferred);
-            });
-            LogoutCommand = new Command(() =>
-            {
-                Preferences.Default.Clear(nameof(PreferenceKeys.Email));
-                Preferences.Default.Clear(nameof(PreferenceKeys.Token));
+            await Browser.Default.OpenAsync(arg, BrowserLaunchMode.SystemPreferred);
+        });
+        LogoutCommand = new Command(() =>
+        {
+            Preferences.Default.Clear();
 
-                if (Application.Current != null)
-                {
-                    _ = Toast.Make("Logged out.").Show();
-                    Application.Current.MainPage = serviceProvider.GetRequiredService<AuthShell>();
-                }
-            });
-        }
+            if (Application.Current != null)
+            {
+                _ = Toast.Make("Logged out.").Show();
+                Application.Current.MainPage = serviceProvider.GetRequiredService<AuthShell>();
+            }
+        });
     }
 }
---
+++ Hybrid/FlyoutFooter.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Hybrid;
+
+public partial class FlyoutFooter : ContentView
+{
+    public FlyoutFooter()
+    {
+        InitializeComponent();
+
+        BindingContext = this;
+    }
+
+    public string CurrentDate => DateTime.UtcNow.ToLongDateString();
+}
