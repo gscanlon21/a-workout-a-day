50db56e95b3b5302a5ade192cbf99c6eb58d0599
Graham Scanlon <git@tscanlon.com>
2023-07-15 10:46:25 -0500

    feat: captcha
    Former-commit-id: e2a555f6105f2be5fa4ccaeab220db939b3adc35

---
+++ Core/Models/Options/CaptchaSettings.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Core.Models.Options;
+
+/// <summary>
+/// App settings for the domain name.
+/// </summary>
+public class CaptchaSettings
+{
+    /// <summary>
+    /// The public site-key for the captcha.
+    /// </summary>
+    public string SiteKey { get; set; } = null!;
+
+    /// <summary>
+    /// The secret api-key for the captcha.
+    /// </summary>
+    public string ApiKey { get; set; } = null!;
+
+    /// <summary>
+    /// The link to the captcha verify api.
+    /// 
+    /// sa. https://api.friendlycaptcha.com/api/v1/siteverify
+    /// </summary>
+    public string ApiLink { get; set; } = null!;
+    public Uri ApiUri => new(ApiLink);
+}
