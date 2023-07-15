7f9ee59c4c99041911691c10bafac81e755c07df
Graham Scanlon <git@tscanlon.com>
2023-07-14 22:16:03 -0500

    improvement: app styles
    Former-commit-id: 8af316050ff3edecf7ac408cbf1d0a10e949d16a

--- Hybrid/Converters/MethodToValueConverter.cs
+++ Hybrid/Converters/MethodToValueConverter.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
+﻿using System.Globalization;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hybrid.Converters;
 
@@ -13,29 +8,35 @@ public sealed class MethodToValueConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        var methodName = parameter as string;
-        if (value == null || methodName == null)
+        if (value == null || parameter is not string methodName)
+        {
             return value;
+        }
+
         var methodInfo = value.GetType().GetMethod(methodName, Type.EmptyTypes);
         if (methodInfo == null)
         {
             methodInfo = GetExtensionMethod(value.GetType(), methodName);
-            if (methodInfo == null) return value;
+            if (methodInfo == null) 
+            {
+                return value;
+            }
+
             return methodInfo.Invoke(null, new[] { value });
         }
+
         return methodInfo.Invoke(value, Array.Empty<object>());
     }
 
     static MethodInfo? GetExtensionMethod(Type extendedType, string methodName)
     {
-        var method = Assembly.GetExecutingAssembly()
+        return Assembly.GetExecutingAssembly()
             .GetTypes()
             .Where(type => !type.IsGenericType && !type.IsNested)
             .SelectMany(type => type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic), (_, method) => method)
             .Where(m => m.IsDefined(typeof(ExtensionAttribute), false))
             .Where(m => m.GetParameters()[0].ParameterType == extendedType)
             .FirstOrDefault(m => m.Name == methodName);
-        return method;
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
