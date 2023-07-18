09b2da0da92aacc12b8a0e2504e4b95a346c9483
Graham Scanlon <git@tscanlon.com>
2023-07-18 17:45:44 -0500

    feat: donation view component
    Former-commit-id: 5457125255dc1343d2de15e7407bdd4d0cfe336a

---
+++ Web/Components/User/DonateViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Components.User;
+
+
+public class DonateViewComponent : ViewComponent
+{
+    /// <summary>
+    /// For routing
+    /// </summary>
+    public const string Name = "Donate";
+
+    public DonateViewComponent() { }
+
+    public async Task<IViewComponentResult> InvokeAsync(Data.Entities.User.User user)
+    {
+        return View("Donate");
+    }
+}
