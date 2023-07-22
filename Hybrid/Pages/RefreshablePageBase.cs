c0f6bc0028fd80f0bd780ad7c58239d9a256c6c1
Graham Scanlon <git@tscanlon.com>
2023-07-22 11:12:07 -0500

    fix(app, #5): pull to refresh

--- Hybrid/Pages/RefreshablePageBase.cs
+++ Hybrid/Pages/RefreshablePageBase.cs
@@ -2,16 +2,23 @@
 
 namespace Hybrid.Pages;
 
-
 public abstract class RefreshablePageBase : ComponentBase
 {
-    public static RefreshablePageBase? Current;
+    public static readonly IDictionary<string, NavigationManager> Current = new Dictionary<string, NavigationManager>();
 
     [Inject]
     public NavigationManager NavigationManager { get; set; } = null!;
 
-    protected RefreshablePageBase()
+    [Parameter]
+    public string? RefreshId { get; set; }
+
+    public override Task SetParametersAsync(ParameterView parameters)
     {
-        Current = this;
+        if (parameters.TryGetValue(nameof(RefreshId), out string? refreshId) && refreshId != null)
+        {
+            Current.TryAdd(refreshId, NavigationManager);
+        }
+
+        return base.SetParametersAsync(parameters);
     }
 }
