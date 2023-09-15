2d63a47955a92e9b54b7e6c3610fa1f6d09ec11c
Graham Scanlon <git@tscanlon.com>
2023-09-14 19:50:00 -0500

    fix(#11): more accurate workouts per week messaging

--- Web/ViewModels/User/Components/WorkoutsPerWeekViewModel.cs
+++ Web/ViewModels/User/Components/WorkoutsPerWeekViewModel.cs
@@ -5,5 +5,25 @@
 /// </summary>
 public class WorkoutsPerWeekViewModel
 {
-    public required Data.Entities.User.User User { get; set; }
+    public Data.Entities.User.User User { get; }
+
+    public int MaxWorkoutsPerWeek { get; }
+
+    public int MinWorkoutsPerWeek { get; }
+
+    public WorkoutsPerWeekViewModel(Data.Entities.User.User user)
+    {
+        User = user;
+
+        (MinWorkoutsPerWeek, MaxWorkoutsPerWeek) = user.Frequency switch
+        {
+            Core.Models.User.Frequency.FullBody2Day => (2, 3),
+            Core.Models.User.Frequency.PushPullLeg3Day => (6, 6),
+            Core.Models.User.Frequency.UpperLowerBodySplit4Day => (4, 6),
+            Core.Models.User.Frequency.UpperLowerFullBodySplit3Day => (3, 4),
+            Core.Models.User.Frequency.PushPullLegsFullBodySplit4Day => (4, 5),
+            Core.Models.User.Frequency.PushPullLegsUpperLowerSplit5Day => (5, 6),
+            _ => (2, 6)
+        };
+    }
 }
