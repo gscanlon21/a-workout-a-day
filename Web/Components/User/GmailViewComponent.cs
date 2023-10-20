41d0dbe2d0576e73378fb4c3b0dbbda85065ba96
Graham Scanlon <git@tscanlon.com>
2023-10-20 08:26:00 -0500

    improvement: move the gmail unsupported messaging into a component on the preferences page instead of limiting sign-up. There's an app now so users aren't required to subscribe to the newsletter.
    Former-commit-id: a2f783400c799221b12ab542cf0bb2d51c072c25

---
+++ Web/Components/User/GmailViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels.User.Components;
+
+namespace Web.Components.User;
+
+public class GmailViewComponent : ViewComponent
+{
+    /// <summary>
+    /// For routing
+    /// </summary>
+    public const string Name = "Gmail";
+
+    public GmailViewComponent() { }
+
+    public async Task<IViewComponentResult> InvokeAsync(Data.Entities.User.User user)
+    {
+        return View("Gmail", new GmailViewModel()
+        {
+            User = user
+        });
+    }
+}
--- Web/ViewModels/User/Components/GmailViewModel.cs
+++ Web/ViewModels/User/Components/GmailViewModel.cs
@@ -1,9 +1,9 @@
 ﻿namespace Web.ViewModels.User.Components;
 
 /// <summary>
-/// Viewmodel for Confirmation.cshtml
+/// Viewmodel for Gmail.cshtml
 /// </summary>
-public class ConfirmationViewModel
+public class GmailViewModel
 {
     public required Data.Entities.User.User User { get; init; } = null!;
 }
