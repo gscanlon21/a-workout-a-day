f0fc93c644280624edd3d5fd7ebc66151fa7bc5f
Graham Scanlon <git@tscanlon.com>
2023-10-22 21:40:00 -0500

    improvement: show count of custom footnotes

--- Web/ViewModels/User/Components/FootnoteViewModel.cs
+++ Web/ViewModels/User/Components/FootnoteViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Web.ViewModels.User.Components;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.ViewModels.User.Components;
 
 /// <summary>
 /// Viewmodel for Confirmation.cshtml
@@ -7,5 +9,7 @@ public class FootnoteViewModel
 {
     public string Token { get; init; } = null!;
     public Data.Entities.User.User User { get; init; } = null!;
+
+    [Display(Name = "Custom Footnotes")]
     public IList<Data.Entities.Footnote.Footnote> Footnotes { get; init; } = null!;
 }
