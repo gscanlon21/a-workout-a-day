bbb1c238186c2d8cf1a230893b2c59beaca93912
Graham Scanlon <git@tscanlon.com>
2023-10-24 20:46:21 -0500

    refactor: split up exercise variation manage pages to make future updates easier
    Former-commit-id: f864350e08cd2c8249ffdca8b38738a8684a153a

---
+++ Web/ViewModels/User/UserManageExerciseVariationViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.ViewModels.User;
+
+/// <summary>
+/// For CRUD actions
+/// </summary>
+public class UserManageExerciseVariationViewModel
+{
+    public required UserManageExerciseViewModel Exercise { get; set; }
+    public required UserManageVariationViewModel? Variation { get; set; }
+
+    public bool? WasUpdated { get; init; }
+}
