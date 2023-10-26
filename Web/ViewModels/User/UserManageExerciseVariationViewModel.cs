db9c4fdc5b75ec9b08fd494728307cdd2c20635f
Graham Scanlon <git@tscanlon.com>
2023-10-25 23:31:36 -0500

    fix: parameter mixup

--- Web/ViewModels/User/UserManageExerciseVariationViewModel.cs
+++ Web/ViewModels/User/UserManageExerciseVariationViewModel.cs
@@ -1,10 +1,14 @@
-﻿namespace Web.ViewModels.User;
+﻿using Core.Models.Newsletter;
+
+namespace Web.ViewModels.User;
 
 /// <summary>
 /// For CRUD actions
 /// </summary>
 public class UserManageExerciseVariationViewModel
 {
+    public record Parameters(Section Section, string Email, string Token, int ExerciseId, int VariationId);
+
     public required UserManageExerciseViewModel Exercise { get; set; }
     public required UserManageVariationViewModel? Variation { get; set; }
 
