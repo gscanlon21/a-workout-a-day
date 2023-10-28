49177757fbde15ddbdb2df9ade2c58de944c6917
Graham Scanlon <git@tscanlon.com>
2023-10-28 17:12:38 -0500

    improvement: disable the client unsupported message when the newsletter is disabled
    Former-commit-id: 542a12f40a0610a2f94702047d9911a6ab79fcd4

---
+++ Web/Components/User/UnsupportedClientViewComponent.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels.User.Components;
+
+namespace Web.Components.User;
+
+public class UnsupportedClientViewComponent : ViewComponent
+{
+    /// <summary>
+    /// For routing
+    /// </summary>
+    public const string Name = "UnsupportedClient";
+
+    public UnsupportedClientViewComponent() { }
+
+    public async Task<IViewComponentResult> InvokeAsync(Data.Entities.User.User user)
+    {
+        var unsupportedCient = GetUnsupportedClient(user);
+        if (unsupportedCient == UnsupportedClientViewModel.UnsupportedClient.None)
+        {
+            return Content("");
+        }
+
+        return View("UnsupportedClient", new UnsupportedClientViewModel()
+        {
+            Client = unsupportedCient
+        });
+    }
+
+    private UnsupportedClientViewModel.UnsupportedClient GetUnsupportedClient(Data.Entities.User.User user)
+    {
+        // If the newsletter is disabled. Don't show the unsupported client message.
+        if (user.NewsletterDisabledReason != null)
+        {
+            return UnsupportedClientViewModel.UnsupportedClient.None;
+        }
+
+        // Gmail does not support `position:absolute` in emails.
+        // https://www.caniemail.com/search/?s=absolute
+        if (user.Email.EndsWith("@gmail.com", StringComparison.OrdinalIgnoreCase))
+        {
+            return UnsupportedClientViewModel.UnsupportedClient.Gmail;
+        }
+
+        return UnsupportedClientViewModel.UnsupportedClient.None;
+    }
+}
---
+++ Web/ViewModels/User/Components/UnsupportedClientViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.ViewModels.User.Components;
+
+/// <summary>
+/// Viewmodel for UnsupportedClients.cshtml
+/// </summary>
+public class UnsupportedClientViewModel 
+{ 
+    public required UnsupportedClient Client { get; init; }
+
+    public enum UnsupportedClient
+    {
+        None = 0,
+
+        [Display(Name = "Gmail")]
+        Gmail = 1,
+    }
+}
