a89539558bf85f41689d8f80f9523d0b2c488356
Graham Scanlon <git@tscanlon.com>
2023-10-28 17:56:51 -0500

    improvement: missing equipment view component messaging

--- Web/Components/User/EquipmentViewComponent.cs
+++ Web/Components/User/EquipmentViewComponent.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core.Code.Extensions;
+using Core.Models.Equipment;
+using Microsoft.AspNetCore.Mvc;
 using Web.ViewModels.User.Components;
 
 namespace Web.Components.User;
-
 
 /// <summary>
 /// Renders an alert box summary of when the suer's next deload week will occur.
@@ -18,9 +19,30 @@ public class EquipmentViewComponent : ViewComponent
 
     public async Task<IViewComponentResult> InvokeAsync(Data.Entities.User.User user)
     {
+        var userEquipmentStatus = GetUserEquipmentStatus(user);
+        if (userEquipmentStatus == EquipmentViewModel.UserEquipmentStatus.None)
+        {
+            return Content("");
+        }
+
         return View("Equipment", new EquipmentViewModel()
         {
-            UserHasEquipment = user.Equipment > 0,
+            Status = userEquipmentStatus,
         });
+    }
+
+    private static EquipmentViewModel.UserEquipmentStatus GetUserEquipmentStatus(Data.Entities.User.User user)
+    {
+        if (user.Equipment == Equipment.None)
+        {
+            return EquipmentViewModel.UserEquipmentStatus.MissingEquipment;
+        }
+
+        if (!user.Equipment.HasAnyFlag32(EquipmentViewModel.ResistanceEquipments))
+        {
+            return EquipmentViewModel.UserEquipmentStatus.MissingResistanceEquipment;
+        }
+
+        return EquipmentViewModel.UserEquipmentStatus.None;
     }
 }
--- Web/ViewModels/User/Components/EquipmentViewModel.cs
+++ Web/ViewModels/User/Components/EquipmentViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace Web.ViewModels.User.Components;
+﻿using Core.Models.Equipment;
+
+namespace Web.ViewModels.User.Components;
 
 public class EquipmentViewModel
 {
-    public bool UserHasEquipment { get; init; }
+    public required UserEquipmentStatus Status { get; init; }
+
+    public enum UserEquipmentStatus
+    {
+        None = 0,
+        MissingEquipment = 1,
+        MissingResistanceEquipment = 2,
+    }
+
+    public const Equipment ResistanceEquipments =
+        Equipment.ResistanceBands
+        | Equipment.GymnasticRings
+        | Equipment.Dumbbells
+        | Equipment.Barbell
+        | Equipment.Kettlebells
+        | Equipment.TRXSystem;
 }
