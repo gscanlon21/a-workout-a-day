e30ba21187c26c48f83c8644937495c59536aac2
Graham Scanlon <git@tscanlon.com>
2023-10-28 17:12:38 -0500

    improvement: disable the client unsupported message when the newsletter is disabled

--- Web/ViewModels/User/Components/GmailViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Web.ViewModels.User.Components;
-
-/// <summary>
-/// Viewmodel for Gmail.cshtml
-/// </summary>
-public class GmailViewModel
-{
-    public required Data.Entities.User.User User { get; init; } = null!;
-}
