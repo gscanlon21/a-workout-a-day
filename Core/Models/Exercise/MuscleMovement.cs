3b6af9b46f6b9e2ad89296e5d4147d087872b089
Graham Scanlon <git@tscanlon.com>
2023-10-28 20:43:26 -0500

    refactor: copy
    Former-commit-id: cf0c82b12111d0e3a6fad07101188833b797b5b8

--- Core/Models/Exercise/MuscleMovement.cs
+++ Core/Models/Exercise/MuscleMovement.cs
@@ -9,19 +9,13 @@ namespace Core.Models.Exercise;
 public enum MuscleMovement
 {
     /// <summary>
-    /// In isometric exercises, the muscles do not shorten or lengthen. 
-    /// These exercises are often referred to as static. 
-    /// For example, a person may hold a plank position and therefore no muscles shorten or lengthen. 
-    /// Other isometric exercise examples are abdominal vacuums and bridges.
+    /// These are strength exercises where your muscles contract while you hold a still position.
     /// </summary>
     [Display(Name = "Isometric")]
     Isometric = 1 << 0, // 1
 
     /// <summary>
-    /// With isotonic exercises, the same amount of muscle tension or weight is maintained throughout the movement. 
-    /// An example is performing a bicep curl as the weight remains the same as the joint moves through the entire range of motion. 
-    /// Other examples of isotonic exercises include push-ups, pull-ups, and squats. 
-    /// The weight of the body remains the same as the joint moves between the start and end ranges.
+    /// Isotonic exercises are those that put a consistent tension on the muscles while moving them through a full range of motion.
     /// </summary>
     [Display(Name = "Isotonic")]
     Isotonic = 1 << 1, // 2
@@ -34,12 +28,7 @@ public enum MuscleMovement
     Plyometric = 1 << 2, // 4
 
     /// <summary>
-    /// Isokinetic exercise involves motion, but the speed stays the same, such as with a stationary bike. 
-    /// The word isokinetic contains kinetic, which means motion. 
-    /// With a stationary bike, the leg moves through the circular motion of the pedal circumferences, but the speed of limb motion and revolutions per minute stays the same. 
-    /// In isokinetic exercises, the resistance can vary, such as using the resistance adjustment in an exercise bike. 
-    /// Often specialized equipment is used to promote isokinetic exercise. Another example is a treadmill. This allows target speeds and resistance to be adjusted and dialed in for the individual. Even allows specific muscle groups to be targeted. 
-    /// Overall, isokinetic exercises can help to improve muscular strength and endurance.
+    /// Isokinetic exercise involves performing movements at a constant speed with varying resistance.
     /// </summary>
     [Display(Name = "Isokinetic")]
     Isokinetic = 1 << 3, // 8
