81851acaa9ad170f314faf633ffb143371792289
Graham Scanlon <git@tscanlon.com>
2023-11-08 19:59:00 -0500

    improvement: prerequisite exercise information
    Former-commit-id: addef0b897202700cb42485b8c20a2f6e7c843c3

--- Web/ViewModels/User/Components/PrerequisiteViewModel.cs
+++ Web/ViewModels/User/Components/PrerequisiteViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Core.Models.Newsletter;
+using Data.Entities.Exercise;
+using Lib.ViewModels.Exercise;
 using Lib.ViewModels.Newsletter;
 using Lib.ViewModels.User;
 
@@ -6,8 +8,17 @@ namespace Web.ViewModels.User.Components;
 
 public class PrerequisiteViewModel
 {
-    public Verbosity Verbosity => Verbosity.Instructions | Verbosity.Images;
+    public Verbosity Verbosity => Verbosity.Instructions | Verbosity.Images | Verbosity.ProgressionBar;
     public required UserNewsletterViewModel UserNewsletter { get; init; }
+    public required IList<ExercisePrerequisite> Prerequisites { get; init; }
     public required IList<ExerciseVariationViewModel> VisiblePrerequisites { get; init; }
     public required IList<ExerciseVariationViewModel> InvisiblePrerequisites { get; init; }
+
+    public class ExerciseSectionComparer : IEqualityComparer<ExerciseVariationViewModel>
+    {
+        public bool Equals(ExerciseVariationViewModel? a, ExerciseVariationViewModel? b)
+            => EqualityComparer<ExerciseViewModel>.Default.Equals(a?.Exercise, b?.Exercise);
+
+        public int GetHashCode(ExerciseVariationViewModel e) => e.Exercise.GetHashCode();
+    }
 }
