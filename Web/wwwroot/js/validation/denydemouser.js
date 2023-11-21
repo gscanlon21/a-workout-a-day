e7e058cae702ea443654b8edd56384f5688332e0
Graham Scanlon <git@tscanlon.com>
2023-11-20 20:40:00 -0500

    improvement: feature tooltip styles
    Former-commit-id: 2bad1563726a57c78e553ba375a4e23fae0720b5

--- Web/wwwroot/js/validation/denydemouser.js
+++ Web/wwwroot/js/validation/denydemouser.js
@@ -1,4 +1,4 @@
-﻿for (const elem of Array.from(document.getElementById('main').querySelectorAll('a:not(.allow-demo), button:not(.allow-demo), input[type="submit"]:not(.allow-demo)'))) {
+﻿for (const elem of Array.from(document.getElementById('main').querySelectorAll('select:not(.allow-demo), a:not(.allow-demo), button:not(.allow-demo), input:not(.allow-demo)'))) {
     elem.setAttribute('disabled', 'disabled');
     elem.style.pointerEvents = 'none';
     if (elem.tagName.toLowerCase() === 'a') {
