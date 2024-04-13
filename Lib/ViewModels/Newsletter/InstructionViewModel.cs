58885044afc63bc72b1d02f2856854c057c5c72a
Graham Scanlon <gscanlon21@users.noreply.github.com>
2024-04-13 12:34:54 -0500

    improvement: friendly instruction exercise names
    Former-commit-id: 7c929df9d613eb7fb02760f1f6d5c80a4bbda871

--- Lib/ViewModels/Newsletter/InstructionViewModel.cs
+++ Lib/ViewModels/Newsletter/InstructionViewModel.cs
@@ -10,23 +10,44 @@ public class InstructionViewModel(Equipment.InstructionViewModel instruction, Us
     public Equipment.InstructionViewModel Instruction { get; } = instruction;
     public User.UserNewsletterViewModel? User { get; } = user;
 
+    /// <summary>
+    /// Gets the friendly instruction display name.
+    /// </summary>
     public string GetDisplayName()
     {
-        var vals = EnumExtensions.GetSingleValues32<Core.Models.Equipment.Equipment>().Where(e => Instruction.Equipment.HasFlag(e));
-        // Disabling the friendly equipment exercise name and moving that over to a title attribute.
-        if (true || string.IsNullOrWhiteSpace(Instruction.Name))
+        var equipmentDisplayName = GetEquipmentDisplayName();
+
+        if (string.IsNullOrWhiteSpace(equipmentDisplayName))
         {
-            if (User == null)
-            {
-                return string.Join(" | ", vals.Select(e => e.GetDisplayName32()));
-            }
-            else
-            {
-                return string.Join(" | ", vals.Where(e => User.Equipment.HasFlag(e)).Select(e => e.GetDisplayName32()));
-            }
+            // Don't throw an exception if null, it's not a fatal error.
+            return Instruction.Name ?? Instruction.Link ?? "ERROR!";
+        }
+        else if (string.IsNullOrWhiteSpace(Instruction.Name))
+        {
+            return equipmentDisplayName;
         }
 
-        // This is the friendly equipment exercise name.
-        return Instruction.Name;
+        return $"{Instruction.Name} ({equipmentDisplayName})";
+    }
+
+    /// <summary>
+    /// Gets the friendly equipment display name.
+    /// </summary>
+    private string? GetEquipmentDisplayName()
+    {
+        var equipments = EnumExtensions.GetSingleValues32<Core.Models.Equipment.Equipment>().Where(e => Instruction.Equipment.HasFlag(e)).ToList();
+        if (equipments.Count == 0)
+        {
+            return null;
+        }
+
+        if (User == null)
+        {
+            return string.Join(" | ", equipments.Select(e => e.GetDisplayName32()));
+        }
+        else
+        {
+            return string.Join(" | ", equipments.Where(e => User.Equipment.HasFlag(e)).Select(e => e.GetDisplayName32()));
+        }
     }
 }
