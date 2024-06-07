faf43ae0eccccde37367ccd5cebe6c78b15bc53b
gscanlon <git@tscanlon.com>
2024-06-06 19:29:34 -0500

    improvement: let users change more preferences
    Former-commit-id: 6c3a4a6b429793c79e8e10ca616dbd64a0e14ad7

--- Core/Consts/ExerciseConsts.cs
+++ Core/Consts/ExerciseConsts.cs
@@ -8,6 +8,4 @@ public static class ExerciseConsts
     /// ~24 per exercise: 6reps * 4sets; 8reps * 3sets; 12reps * 2sets; 60s total TUT / 2.5.
     /// </summary>
     public const int TargetVolumePerExercise = 24;
-
-    public const Section SectionsToRefresh = Section.Main | Section.Sports;
 }
