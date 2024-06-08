f6cb62f2e083929dddf752f06bf6a84c2d4ea9eb
gscanlon <git@tscanlon.com>
2024-06-08 14:52:49 -0500

    refactor: use view model
    Former-commit-id: 825afacfe5224a72b323335a3fbc0d00ce857296

--- Web/ViewModels/User/Components/MuscleTargetsViewModel.cs
+++ Web/ViewModels/User/Components/MuscleTargetsViewModel.cs
@@ -43,16 +43,36 @@ public class MuscleTargetsViewModel
 
     public class MonthlyMuscle
     {
+        public double MaxRangeValue => UserMuscleStrength.MuscleTargets.Values.Max(r => r.End.Value);
+
         public required MuscleGroups MuscleGroup { get; init; }
-        public required bool ShowButtons { get; init; }
         public required Range UserMuscleTarget { get; init; }
+
         public required double Start { get; init; }
         public double Middle { get; init; }
         public required double End { get; init; }
+
         public required double DefaultStart { get; init; }
         public required double DefaultEnd { get; init; }
+
         public required double ValueInRange { get; init; }
         public bool IsMinVolumeInRange => ValueInRange >= Start;
         public bool IsMaxVolumeInRange => ValueInRange <= End;
+
+        public required bool ShowButtons { get; init; }
+        public bool ShowDecreaseStart => ShowButtons && UserMuscleTarget.Start.Value > 0;
+        public bool ShowIncreaseStart => ShowButtons && UserMuscleTarget.Start.Value + UserConsts.IncrementMuscleTargetBy < UserMuscleTarget.End.Value - UserConsts.IncrementMuscleTargetBy;
+        public bool ShowDecreaseEnd => ShowButtons && UserMuscleTarget.End.Value - UserConsts.IncrementMuscleTargetBy > UserMuscleTarget.Start.Value + UserConsts.IncrementMuscleTargetBy;
+        public bool ShowIncreaseEnd => ShowButtons && UserMuscleTarget.End.Value < MaxRangeValue;
+
+        public string Color => (IsMinVolumeInRange, IsMaxVolumeInRange, ShowButtons) switch
+        {
+            // Max volume is out of range
+            (_, false, true) => "orangered",
+            // Min volume is out of range
+            (false, _, true) => "darkorange",
+            // Volume is in range
+            _ => "limegreen",
+        };
     }
 }
