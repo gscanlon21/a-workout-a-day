ba15b3666d05f29d240cd1ebfdf8b233b535e296
gscanlon <git@tscanlon.com>
2024-06-16 18:46:04 -0500

    improvemenet: intensity
    Former-commit-id: f3434ef3466994dd9e9eb981284c195c45faf27f

--- Core/Consts/UserConsts.cs
+++ Core/Consts/UserConsts.cs
@@ -78,8 +78,7 @@ public class UserConsts
 
     public const Intensity IntensityDefault = Intensity.Light;
 
-    public const Verbosity VerbosityDefault = Verbosity.Instructions
-        | Verbosity.Images | Verbosity.ProgressionBar | Verbosity.Proficiency;
+    public const Verbosity VerbosityDefault = Verbosity.Instructions | Verbosity.Images | Verbosity.ProgressionBar;
 
     public const FootnoteType FootnotesDefault = FootnoteType.FitnessTips | FootnoteType.FitnessFacts
         | FootnoteType.HealthTips | FootnoteType.HealthFacts | FootnoteType.GoodVibes | FootnoteType.Mindfulness;
