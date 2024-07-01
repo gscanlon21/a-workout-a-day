859da52887b91632d31da42661d1b8b2c591deba
gscanlon <git@tscanlon.com>
2024-07-01 15:35:50 -0500

    improvement: rehab focus warning
    Former-commit-id: 93415c3c6ed6591d03de77f88cddbe6f19fec285

---
+++ Web/Components/User/RehabViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Core.Models.Exercise;
+using Microsoft.AspNetCore.Mvc;
+using Web.Views.Shared.Components.Rehab;
+
+namespace Web.Components.User;
+
+/// <summary>
+/// Displays a warning when the user has a Rehab Focus group selected, 
+/// since that causes the workouts to exclude that muscle group.
+/// </summary>
+public class RehabViewComponent : ViewComponent
+{
+    /// <summary>
+    /// For routing
+    /// </summary>
+    public const string Name = "Rehab";
+
+    public RehabViewComponent() { }
+
+    public async Task<IViewComponentResult> InvokeAsync(Data.Entities.User.User user)
+    {
+        if (user.RehabFocus == RehabFocus.None)
+        {
+            return Content("");
+        }
+
+        return View("Rehab", new RehabViewModel()
+        {
+            RehabFocus = user.RehabFocus
+        });
+    }
+}
---
+++ Web/Views/Shared/Components/Rehab/Rehab.cshtml.cs
@@ -0,0 +1,8 @@
+using Core.Models.Exercise;
+
+namespace Web.Views.Shared.Components.Rehab;
+
+public class RehabViewModel
+{
+    public required RehabFocus RehabFocus { get; init; }
+}
