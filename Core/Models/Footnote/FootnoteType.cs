81224d5c40c7f3a05b278947246c9227f0c8dbb4
gscanlon <git@tscanlon.com>
2024-07-05 18:49:26 -0500

    improvement: footnotes

--- Core/Models/Footnote/FootnoteType.cs
+++ Core/Models/Footnote/FootnoteType.cs
@@ -13,7 +13,7 @@ public enum FootnoteType
     /// 
     /// sa. Take five to 10 minutes to warm up and cool down properly.
     /// </summary>
-    [Display(Name = "Fitness Tips", Description = "sa. Take five to 10 minutes to warm up and cool down properly.")]
+    [Display(Name = "Fitness Tips")]
     FitnessTips = 1 << 0, // 1
 
     /// <summary>
@@ -21,19 +21,19 @@ public enum FootnoteType
     /// 
     /// sa. Practicing everyday mindfulness can improve your memory and concentration skills....
     /// </summary>
-    [Display(Name = "Health Tips", Description = "sa. Practicing everyday mindfulness can improve your memory and concentration skills...")]
+    [Display(Name = "Health Tips")]
     HealthTips = 1 << 1, // 2
 
     /// <summary>
     /// User defined footnotes.
     /// </summary>
-    [Display(Name = "Fitness Facts", Description = "sa. Adding yoga to aerobic exercise may help lower high blood pressure.")]
+    [Display(Name = "Fitness Facts")]
     FitnessFacts = 1 << 2, // 4
 
     /// <summary>
     /// User defined footnotes.
     /// </summary>
-    [Display(Name = "Health Facts", Description = "sa. Waking up earlier is healthier than going to bed later when trying to limit the damage of sleep deprivation.")]
+    [Display(Name = "Health Facts")]
     HealthFacts = 1 << 3, // 8
 
     /// <summary>
@@ -41,56 +41,40 @@ public enum FootnoteType
     /// 
     /// sa. Never give up!
     /// </summary>
-    [Display(Name = "Fitness Motivation", Description = "sa. Never give up!")]
+    [Display(Name = "Fitness Motivation")]
     FitnessMotivation = 1 << 4, // 16
-
-    /// <summary>
-    /// Life motivation.
-    /// 
-    /// sa. Never give up!
-    /// </summary>
-    [Display(Name = "Life Motivation", Description = "sa. Never give up!")]
-    LifeMotivation = 1 << 5, // 32
 
     /// <summary>
     /// Fitness affirmations. 
     /// 
     /// sa. I'm getting stronger after every workout
     /// </summary>
-    [Display(Name = "Fitness Affirmations", Description = "sa. I'm getting stronger after every workout.")]
-    FitnessAffirmations = 1 << 6, // 64
-
-    /// <summary>
-    /// Life affirmations. 
-    /// 
-    /// sa. I'm a thoughtful and interesting person.
-    /// </summary>
-    [Display(Name = "Life Affirmations", Description = "sa. I'm a thoughtful and interesting person.")]
-    LifeAffirmations = 1 << 7, // 128
+    [Display(Name = "Fitness Affirmations")]
+    FitnessAffirmations = 1 << 5, // 32
 
     /// <summary>
     /// Mindfulness
     /// 
     /// sa. Breathe deeply. You are in the present moment.
     /// </summary>
-    [Display(Name = "Mindfulness", Description = "sa. Breathe deeply. You are in the present moment.")]
-    Mindfulness = 1 << 8, // 256
+    [Display(Name = "Mindfulness")]
+    Mindfulness = 1 << 6, // 64
 
     /// <summary>
     /// Good vibes. Re-parenting.
     /// 
     /// sa. You are beautiful!
     /// </summary>
-    [Display(Name = "Good Vibes", Description = "sa. You are beautiful!")]
-    GoodVibes = 1 << 9, // 512
+    [Display(Name = "Good Vibes")]
+    GoodVibes = 1 << 7, // 128
 
     /// <summary>
     /// User defined footnotes.
     /// </summary>
-    [Display(Name = "Custom", Description = "sa. You are beautiful!")]
-    Custom = 1 << 10, // 1024
+    [Display(Name = "Custom")]
+    Custom = 1 << 8, // 256
 
-    System = All & ~Custom, // FitnessTips | FitnessFacts | FitnessMotivation | FitnessAffirmations | HealthTips | HealthFacts | LifeMotivation | LifeAffirmations | GoodVibes | Mindfulness
+    System = All & ~Custom, // FitnessTips | FitnessFacts | FitnessMotivation | FitnessAffirmations | HealthTips | HealthFacts | GoodVibes | Mindfulness
 
-    All = FitnessTips | FitnessFacts | FitnessMotivation | FitnessAffirmations | HealthTips | HealthFacts | LifeMotivation | LifeAffirmations | GoodVibes | Mindfulness | Custom
+    All = FitnessTips | FitnessFacts | FitnessMotivation | FitnessAffirmations | HealthTips | HealthFacts | GoodVibes | Mindfulness | Custom
 }
