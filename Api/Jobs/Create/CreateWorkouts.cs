a573ef1928f9010cca6543d86cc70627f126cf0e
gscanlon <git@tscanlon.com>
2024-07-19 12:40:49 -0500

    chore: update packages

--- Api/Jobs/Create/CreateWorkouts.cs
+++ Api/Jobs/Create/CreateWorkouts.cs
@@ -39,6 +39,7 @@ public class CreateWorkouts : IJob, IScheduled
     {
         try
         {
+            _logger.Log(LogLevel.Information, "Starting job {p0}", nameof(CreateWorkouts));
             var options = new ParallelOptions() { MaxDegreeOfParallelism = 3, CancellationToken = context.CancellationToken };
             await Parallel.ForEachAsync(await GetUsers(), options, async (userToken, cancellationToken) =>
             {
