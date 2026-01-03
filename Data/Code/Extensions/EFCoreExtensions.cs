9c15de4f38ec5c8e43645a8347c3b54f599fb5dd
Graham Scanlon <git@ascallion.com>
2026-01-02 19:18:41 -0600

    improvement: foreign key violation

--- Data/Code/Extensions/EFCoreExtensions.cs
+++ Data/Code/Extensions/EFCoreExtensions.cs
@@ -8,4 +8,15 @@ public static class EFCoreExtensions
     {
         return dbUpdateException.InnerException?.Message.Contains("duplicate key", StringComparison.OrdinalIgnoreCase) ?? false;
     }
+
+    public static bool IsForeignKeyException(this DbUpdateException dbUpdateException, string? tableName = null)
+    {
+        if (!string.IsNullOrWhiteSpace(tableName))
+        {
+            return (dbUpdateException.InnerException?.Message.Contains($"\"{tableName}\"", StringComparison.OrdinalIgnoreCase) ?? false)
+                && (dbUpdateException.InnerException?.Message.Contains("foreign key", StringComparison.OrdinalIgnoreCase) ?? false);
+        }
+
+        return dbUpdateException.InnerException?.Message.Contains("foreign key", StringComparison.OrdinalIgnoreCase) ?? false;
+    }
 }
