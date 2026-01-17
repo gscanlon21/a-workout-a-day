ffe03a04c147713b0f4872f1b883b300a885fe9e
Graham Scanlon <git@ascallion.com>
2026-01-17 12:26:30 -0600

    refactor: swap to collectionview

---
+++ Hybrid/Converters/SelectionChangedEventArgsConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Hybrid.Converters;
+
+public sealed class SelectionChangedEventArgsConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is SelectionChangedEventArgs args && args.CurrentSelection.Count > 0)
+        {
+            return args.CurrentSelection[0];
+        }
+
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
