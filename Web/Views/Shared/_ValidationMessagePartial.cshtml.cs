080c8ccb1097458ead36c53db5ba0e011d69521d
Graham Scanlon <git@ascallion.com>
2026-02-12 17:01:02 -0600

    improvement: scope to tab

--- Web/Views/Shared/_ValidationMessagePartial.cshtml.cs
+++ Web/Views/Shared/_ValidationMessagePartial.cshtml.cs
@@ -11,4 +11,6 @@ public class ValidationMessagePartialModel
 
     public const string DefaultFailureMessage = "Something went wrong.";
     public const string DefaultSuccessMessage = "Your preferences have been saved. Changes will be reflected soon.";
+
+    public string Id { get; } = Guid.NewGuid().ToString();
 }
