e8afca83bd5ea273a1c41cab386618e7d718bc00
Graham Scanlon <git@ascallion.com>
2026-04-05 10:07:02 -0500

    perf: index

---
+++ Data/Migrations/20260405150428_AddUserWorkoutPerf.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserWorkoutPerf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_user_workout_UserId_Date",
+                table: "user_workout",
+                columns: new[] { "UserId", "Date" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_user_workout_UserId_Date",
+                table: "user_workout");
+        }
+    }
+}
