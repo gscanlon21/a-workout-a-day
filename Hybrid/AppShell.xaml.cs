cb1ef793344bb046873feedf399ff72b1794e397
Graham Scanlon <git@ascallion.com>
2026-06-11 10:45:20 -0500

    improvement: status bar colors

--- Hybrid/AppShell.xaml.cs
+++ Hybrid/AppShell.xaml.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Maui.Alerts;
+using Hybrid.Database;
 using Microsoft.Extensions.Options;
 using System.Windows.Input;
 
@@ -18,26 +19,39 @@ public partial class AppShell : Shell
     }
 }
 
-public class AppShellViewModel(IOptions<SiteSettings> siteSettings, IServiceProvider serviceProvider)
+public class AppShellViewModel
 {
     /// <summary>
     /// Used in the view.
     /// </summary>
-    public IOptions<SiteSettings> SiteSettings { get; set; } = siteSettings;
+    public IOptions<SiteSettings> SiteSettings { get; private set; }
 
-    public ICommand BrowserCommand { private set; get; } = new Command<string>(async (string arg) =>
+    public AppShellViewModel(IOptions<SiteSettings> siteSettings, UserPreferences preferences, IServiceProvider serviceProvider)
+    {
+        SiteSettings = siteSettings;
+
+        PreferencesCommand = new Command(async () =>
+        {
+            await Browser.Default.OpenAsync($"{siteSettings.Value.WebLink}/u/{Uri.EscapeDataString(preferences.Email.Value)}?token={Uri.EscapeDataString(preferences.Token.Value)}", BrowserLaunchMode.SystemPreferred);
+        });
+
+        LogoutCommand = new Command(() =>
+        {
+            Preferences.Default.Clear();
+
+            if (Application.Current != null)
+            {
+                _ = Toast.Make("Logged out.").Show();
+                Application.Current.MainPage = serviceProvider.GetRequiredService<AuthShell>();
+            }
+        });
+    }
+
+    public ICommand LogoutCommand { private set; get; }
+    public ICommand PreferencesCommand { private set; get; }
+
+    public ICommand BrowserCommand { private set; get; } = new Command<string>(async (arg) =>
     {
         await Browser.Default.OpenAsync(arg, BrowserLaunchMode.SystemPreferred);
-    });
-
-    public ICommand LogoutCommand { private set; get; } = new Command(() =>
-    {
-        Preferences.Default.Clear();
-
-        if (Application.Current != null)
-        {
-            _ = Toast.Make("Logged out.").Show();
-            Application.Current.MainPage = serviceProvider.GetRequiredService<AuthShell>();
-        }
     });
 }
