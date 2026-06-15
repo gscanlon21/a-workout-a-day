aebe9580ee7f2d13b6721ddf0449648aa0619d0e
Graham Scanlon <git@ascallion.com>
2026-06-15 08:07:21 -0500

    fix: order

--- Data/Query/Filters/ExerciseQueryFilter.cs
+++ Data/Query/Filters/ExerciseQueryFilter.cs
@@ -1,6 +1,7 @@
 ﻿using Data.Query.Options;
 using Data.Query.Options.Users;
 using Microsoft.Extensions.DependencyInjection;
+using static Core.Code.Extensions.EnumerableExtensions;
 using static Data.Query.Runners.BaseQueryRunner;
 
 namespace Data.Query.Filters;
@@ -20,6 +21,10 @@ public class ExerciseQueryFilter : BaseQueryFilter
 
     public override async Task<List<QueryResults>> Filter(List<InProgressQueryResults> queryResults, IServiceScopeFactory factory, OrderBy orderBy = OrderBy.None, int take = int.MaxValue)
     {
-        return queryResults.Select(r => new QueryResults(section, r.Exercise, r.Variation, r.UserExercise, r.UserVariation, r.Prerequisites, r.Postrequisites, r.EasierVariation, r.HarderVariation, UserOptions.Intensity)).ToList();
+        return queryResults.Select(r => new QueryResults(section, r.Exercise, r.Variation, r.UserExercise, r.UserVariation, r.Prerequisites, r.Postrequisites, r.EasierVariation, r.HarderVariation, UserOptions.Intensity))
+            .OrderBy(vm => vm.Variation.Progression.Min, NullOrder.NullsFirst)
+            .ThenBy(vm => vm.Variation.Progression.Max, NullOrder.NullsLast)
+            .ThenBy(vm => vm.Variation.Name)
+            .ToList();
     }
 }
